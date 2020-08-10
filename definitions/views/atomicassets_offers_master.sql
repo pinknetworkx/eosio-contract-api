f794b916d3fad919a2620c2e8674333004222831
Fabian Emilius <fabian.emilius@tum.de>
2020-08-10 18:20:02 +0200

    Remove whitelist and blacklists from API

--- definitions/views/atomicassets_offers_master.sql
+++ definitions/views/atomicassets_offers_master.sql
@@ -4,8 +4,6 @@ CREATE OR REPLACE VIEW atomicassets_offers_master AS
         offer_a.sender sender_name, offer_a.recipient recipient_name, offer_a.memo,
         offer_a.state,
 
-        code_recipient.account recipient_contract_account,
-
         EXISTS(SELECT * FROM contract_codes WHERE account = offer_a.sender) is_sender_contract,
         EXISTS(SELECT * FROM contract_codes WHERE account = offer_a.recipient) is_recipient_contract,
 
