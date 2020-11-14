a0bc7c729961e27b5cf5a3ffbb99a7eeb5f3c401
fabian-emilius <fabian.emilius@gmail.com>
2020-11-14 17:29:55 +0100

    Log timeout notice even if query is canceled

--- definitions/views/atomicmarket_template_prices_master.sql
+++ definitions/views/atomicmarket_template_prices_master.sql
@@ -1,12 +1,42 @@
 CREATE OR REPLACE VIEW atomicmarket_template_prices_master AS
     SELECT
-    	sale.market_contract, asset.contract assets_contract, asset.collection_name, asset.template_id,
-    	sale.settlement_symbol symbol, PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY sale.final_price) median,
-    	AVG(sale.final_price)::bigint average, MIN(sale.final_price) "min", MAX(sale.final_price) "max", COUNT(*) sales
-    FROM
-    	atomicassets_assets asset, atomicassets_offers_assets offer_asset, atomicmarket_sales sale
-    WHERE
-    	sale.assets_contract = offer_asset.contract AND sale.offer_id = offer_asset.offer_id AND
-    	offer_asset.contract = asset.contract AND offer_asset.asset_id = asset.asset_id AND
-    	asset.template_id IS NOT NULL AND sale.state = 3
-    GROUP BY sale.market_contract, sale.settlement_symbol, asset.contract, asset.collection_name, asset.template_id
+        t2.market_contract, t2.assets_contract, t2.collection_name, t2.template_id, t2.symbol,
+        PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY t2.price) median,
+        AVG(t2.price)::bigint average,
+        PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY t2.price) FILTER (WHERE t2.number <= 5 OR t2."time" / 1000 >= extract(epoch from now()) - 3600 * 24 * 7) suggested_median,
+        (AVG(t2.price) FILTER (WHERE t2.number <= 5 OR t2."time" / 1000 >= extract(epoch from now()) - 3600 * 24 * 7))::bigint suggested_average,
+        MIN(t2.price) "min", MAX(t2.price) "max", COUNT(*) sales
+    FROM (
+        SELECT
+            t1.*, row_number() OVER (PARTITION BY t1.assets_contract, t1.collection_name, t1.template_id ORDER BY t1."time" DESC) "number"
+        FROM (
+            (
+                SELECT
+                    sale.market_contract, MIN(asset.contract) assets_contract,
+                    sale.collection_name, MIN(asset.template_id) template_id,
+                    sale.settlement_symbol symbol, sale.final_price price, sale.updated_at_time "time"
+                FROM
+                    atomicassets_assets asset, atomicassets_offers_assets offer_asset, atomicmarket_sales sale
+                WHERE
+                    sale.assets_contract = offer_asset.contract AND sale.offer_id = offer_asset.offer_id AND
+                    offer_asset.contract = asset.contract AND offer_asset.asset_id = asset.asset_id AND
+                    asset.template_id IS NOT NULL AND sale.final_price IS NOT NULL AND sale.state = 3
+                GROUP BY sale.market_contract, sale.sale_id
+                HAVING COUNT(*) = 1
+            ) UNION ALL (
+                SELECT
+                    auction.market_contract, MIN(asset.contract) assets_contract,
+                    auction.collection_name, MIN(asset.template_id) template_id,
+                    auction.token_symbol symbol, auction.price, (auction.end_time * 1000) "time"
+                FROM
+                    atomicassets_assets asset, atomicmarket_auctions_assets auction_asset, atomicmarket_auctions auction
+                WHERE
+                    auction.assets_contract = auction_asset.assets_contract AND auction.auction_id = auction_asset.auction_id AND
+                    auction_asset.assets_contract = asset.contract AND auction_asset.asset_id = asset.asset_id AND
+                    asset.template_id IS NOT NULL AND auction.buyer IS NOT NULL AND auction.state = 1 AND auction.end_time < extract(epoch from now())
+                GROUP BY auction.market_contract, auction.auction_id
+                HAVING COUNT(*) = 1
+            )
+        ) t1
+    ) t2
+    GROUP BY t2.market_contract, t2.assets_contract, t2.collection_name, t2.template_id, t2.symbol
--- src/api/namespaces/atomicmarket/routes/prices.ts
+++ src/api/namespaces/atomicmarket/routes/prices.ts
@@ -3,7 +3,6 @@ import * as express from 'express';
 import { AtomicMarketNamespace } from '../index';
 import { HTTPServer } from '../../../server';
 import { getOpenAPI3Responses, paginationParameters, primaryBoundaryParameters } from '../../../docs';
-import { SaleState } from '../../../../filler/handlers/atomicmarket';
 import { buildBoundaryFilter, filterQueryArgs } from '../../utils';
 import { buildAssetQueryCondition } from '../../atomicassets/routes/assets';
 import {
@@ -20,53 +19,77 @@ export function pricesEndpoints(core: AtomicMarketNamespace, server: HTTPServer,
                 collection_name: {type: 'string', min: 1},
                 template_id: {type: 'int', min: 1},
                 schema_name: {type: 'string', min: 1},
-                symbol: {type: 'string', min: 1},
-                state: {type: 'int', min: 0, default: SaleState.SOLD.valueOf()}
+                symbol: {type: 'string', min: 1}
             });
 
-            let queryString = 'SELECT ' +
-                'sale.sale_id, sale.final_price price, mint.min_template_mint template_mint, ' +
-                'symbol.token_symbol, symbol.token_precision, ' +
-                'symbol.token_contract, sale.updated_at_time block_time, sale.updated_at_block block_num ' +
-                'FROM ' +
-                    'atomicmarket_sales sale LEFT JOIN atomicmarket_sale_mints mint ON (sale.market_contract = mint.market_contract AND sale.sale_id = mint.sale_id), ' +
-                    'atomicmarket_tokens symbol ' +
-                'WHERE sale.market_contract = $1 AND sale.state = $2 AND ' +
-                'sale.market_contract = symbol.market_contract AND sale.settlement_symbol = symbol.token_symbol ';
-            const queryValues = [core.args.atomicmarket_account, args.state];
+            let subQueryString = 'AND listing.market_contract = $1 ';
+            const queryValues = [core.args.atomicmarket_account];
             let varCounter = queryValues.length;
 
             if (args.collection_name) {
-                queryString += 'AND sale.collection_name = $' + ++varCounter + ' ';
-                queryValues.push(args.collection_name);
+                subQueryString += 'AND asset.collection_name = ANY($' + ++varCounter + ') ';
+                queryValues.push(args.collection_name.split(','));
             }
 
             if (args.schema_name) {
-                queryString += 'AND NOT EXISTS (' +
-                    'SELECT * FROM atomicassets_offers_assets offer_asset, atomicassets_assets asset ' +
-                    'WHERE asset.contract = offer_asset.contract AND asset.asset_id = offer_asset.asset_id AND offer_asset.offer_id = sale.offer_id AND ' +
-                    'asset.schema_name != $' + ++varCounter + ' ' +
-                    ') ';
-
-                queryValues.push(args.schema_name);
+                subQueryString += 'AND asset.schema_name = ANY($' + ++varCounter + ') ';
+                queryValues.push(args.schema_name.split(','));
             }
 
             if (args.template_id) {
-                queryString += 'AND NOT EXISTS (' +
-                    'SELECT * FROM atomicassets_offers_assets offer_asset, atomicassets_assets asset ' +
-                    'WHERE asset.contract = offer_asset.contract AND asset.asset_id = offer_asset.asset_id AND offer_asset.offer_id = sale.offer_id AND ' +
-                    '(asset.template_id != $' + ++varCounter + ' OR asset.template_id IS NULL) ' +
-                    ') ';
-
+                subQueryString += 'AND asset.template_id = ANY($' + ++varCounter + ') ';
                 queryValues.push(args.template_id);
             }
 
             if (args.symbol) {
-                queryString += 'AND sale.settlement_symbol = $' + ++varCounter + ' ';
-                queryValues.push(args.symbol);
+                subQueryString += 'AND symbol.token_symbol = ANY($' + ++varCounter + ') ';
+                queryValues.push(args.symbol.split(','));
             }
 
-            queryString += 'ORDER BY updated_at_block DESC LIMIT 500';
+            let queryString = `
+            SELECT * FROM (
+                (
+                    SELECT
+                        listing.market_contract, MIN(asset.contract) assets_contract, NULL auction_id, listing.sale_id,
+                        listing.collection_name, MIN(asset.schema_name) "schema_name", MIN(asset.template_id) template_id,
+                        MIN(mint.min_template_mint) template_mint,
+                        MIN(symbol.token_symbol) token_symbol, MIN(symbol.token_precision) token_precision, MIN(symbol.token_contract) token_contract, 
+                        listing.final_price price, listing.updated_at_time block_time
+                    FROM
+                        atomicassets_assets asset, atomicassets_offers_assets offer_asset, atomicmarket_sales listing, 
+                        atomicmarket_sale_mints mint, atomicmarket_tokens symbol
+                    WHERE
+                        listing.assets_contract = offer_asset.contract AND listing.offer_id = offer_asset.offer_id AND
+                        listing.market_contract = mint.market_contract AND listing.sale_id = mint.sale_id AND
+                        listing.market_contract = symbol.market_contract AND listing.settlement_symbol = symbol.token_symbol AND
+                        offer_asset.contract = asset.contract AND offer_asset.asset_id = asset.asset_id AND
+                        asset.template_id IS NOT NULL AND listing.final_price IS NOT NULL AND listing.state = 3
+                        ${subQueryString}
+                    GROUP BY listing.market_contract, listing.sale_id
+                    HAVING COUNT(*) = 1
+                ) UNION ALL (
+                    SELECT
+                        listing.market_contract, MIN(asset.contract) assets_contract,  listing.auction_id, NULL sale_id,
+                        listing.collection_name, MIN(asset.schema_name) "schema_name", MIN(asset.template_id) template_id,
+                        MIN(mint.min_template_mint) template_mint,
+                        MIN(symbol.token_symbol) token_symbol, MIN(symbol.token_precision) token_precision, MIN(symbol.token_contract) token_contract, 
+                        listing.price, (listing.end_time * 1000) block_time
+                    FROM
+                        atomicassets_assets asset, atomicmarket_auctions_assets auction_asset, atomicmarket_auctions listing, 
+                        atomicmarket_auction_mints mint, atomicmarket_tokens symbol
+                    WHERE
+                        listing.assets_contract = auction_asset.assets_contract AND auction.auction_id = auction_asset.auction_id AND
+                        mint.market_contract = listing.market_contract AND mint.auction_id = listing.auction_id AND
+                        listing.market_contract = symbol.market_contract AND listing.token_symbol = symbol.token_symbol AND
+                        listing.assets_contract = asset.contract AND auction_asset.asset_id = asset.asset_id AND
+                        asset.template_id IS NOT NULL AND listing.buyer IS NOT NULL AND listing.state = 1 AND listing.end_time < extract(epoch from now())
+                        ${subQueryString}
+                    GROUP BY listing.market_contract, listing.auction_id
+                    HAVING COUNT(*) = 1
+                )
+            ) t1
+            ORDER BY t1.block_time DESC LIMIT 500
+            `;
 
             const prices = await server.query(queryString, queryValues);
 
@@ -95,7 +118,8 @@ export function pricesEndpoints(core: AtomicMarketNamespace, server: HTTPServer,
             let queryString = 'SELECT price.market_contract, price.assets_contract, ' +
                     'price.collection_name, price.template_id, ' +
                     'token.token_symbol, token.token_contract, token.token_precision, ' +
-                    'price."median", price."average", price."min", price."max", price.sales ' +
+                    'price."median", price."average", price."min", price."max", price.sales, ' +
+                    'price.suggested_median, price.suggested_average ' +
                 'FROM atomicassets_templates "template", atomicmarket_template_prices "price", atomicmarket_tokens "token" ' +
                 'WHERE "template".contract = "price".assets_contract AND "template".collection_name = "price".collection_name AND "template".template_id = "price".template_id AND ' +
                     '"price".market_contract = "token".market_contract AND "price".symbol = "token".token_symbol AND ' +
@@ -143,7 +167,8 @@ export function pricesEndpoints(core: AtomicMarketNamespace, server: HTTPServer,
     router.all('/v1/prices/assets', server.web.caching(), async (req, res) => {
         try {
             let queryString = 'SELECT token.token_symbol, token.token_precision, token.token_contract, ' +
-                    'SUM(price."median") "median", SUM(price."average") "average", SUM(price."min") "min", SUM(price."max") "max" ' +
+                    'SUM(price."median") "median", SUM(price."average") "average", SUM(price."min") "min", SUM(price."max") "max", ' +
+                    'SUM(price.suggested_median) suggested_median, SUM(price.suggested_average) suggested_average' +
                 'FROM atomicassets_assets asset, atomicassets_templates "template", atomicmarket_template_prices "price", atomicmarket_tokens token ' +
                 'WHERE asset.contract = template.contract AND asset.template_id = template.template_id AND ' +
                     'template.contract = price.assets_contract AND template.template_id = price.template_id AND ' +
@@ -201,13 +226,6 @@ export function pricesEndpoints(core: AtomicMarketNamespace, server: HTTPServer,
                             description: 'Token symbol',
                             required: false,
                             schema: {type: 'string'}
-                        },
-                        {
-                            name: 'state',
-                            in: 'query',
-                            description: ' 1: Listed; 3: Sold',
-                            required: false,
-                            schema: {type: 'integer', default: 3}
                         }
                     ],
                     responses: getOpenAPI3Responses([500, 200], {
@@ -257,6 +275,8 @@ export function pricesEndpoints(core: AtomicMarketNamespace, server: HTTPServer,
 
                                 average: {type: 'string'},
                                 median: {type: 'string'},
+                                suggested_average: {type: 'string'},
+                                suggested_median: {type: 'string'},
                                 min: {type: 'string'},
                                 max: {type: 'string'}
                             }
@@ -302,6 +322,8 @@ export function pricesEndpoints(core: AtomicMarketNamespace, server: HTTPServer,
                                 token_contract: {type: 'string'},
                                 median: {type: 'string'},
                                 average: {type: 'string'},
+                                suggested_average: {type: 'string'},
+                                suggested_median: {type: 'string'},
                                 min: {type: 'string'},
                                 max: {type: 'string'}
                             }
