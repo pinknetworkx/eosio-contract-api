48d4681698861395db8e26d209f078cd6fec8101
Fabian Emilius <fabian.emilius@tum.de>
2021-02-12 07:31:54 +0100

    Add env variables for connections

--- src/connections/manager.ts
+++ src/connections/manager.ts
@@ -11,24 +11,28 @@ export default class ConnectionManager {
     readonly database: PostgresConnection;
 
     constructor(private readonly config: IConnectionsConfig) {
-        this.chain = new ChainApi(config.chain.http, config.chain.name, config.chain.chain_id);
+        this.chain = new ChainApi(
+            process.env.CHAIN_HTTP || config.chain.http,
+            process.env.CHAIN_NAME || config.chain.name,
+            process.env.CHAIN_ID || config.chain.chain_id
+        );
 
         this.redis = new RedisConnection(
-            config.redis.host,
-            config.redis.port
+            process.env.REDIS_HOST || config.redis.host,
+            parseInt(process.env.REDIS_PORT, 10) || config.redis.port
         );
 
         this.database = new PostgresConnection(
-            config.postgres.host,
-            config.postgres.port,
-            config.postgres.user,
-            config.postgres.password,
-            config.postgres.database
+            process.env.POSTGRES_HOST || config.postgres.host,
+            parseInt(process.env.POSTGRES_PORT, 10) || config.postgres.port,
+            process.env.POSTGRES_USER || config.postgres.user,
+            process.env.POSTGRES_PASSWORD || config.postgres.password,
+            process.env.POSTGRES_DATABASE || config.postgres.database
         );
     }
 
     createShipBlockReader(options?: IBlockReaderOptions): StateHistoryBlockReader {
-        const reader = new StateHistoryBlockReader(this.config.chain.ship);
+        const reader = new StateHistoryBlockReader(process.env.CHAIN_SHIP || this.config.chain.ship);
 
         if (options) {
             reader.setOptions(options);
