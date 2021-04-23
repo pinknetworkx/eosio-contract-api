29757f17d87cb9de0a9c60af419af549fea9cf9d
Fabian Emilius <fabian.emilius@tum.de>
2021-04-24 00:00:37 +0200

    Remove if exists in reader migration scripts

--- definitions/tables/atomicassets_migrate.sql
+++ definitions/tables/atomicassets_migrate.sql
@@ -1,1 +1,1 @@
-CREATE INDEX IF NOT EXISTS atomicassets_transfers_assets_asset_id ON atomicassets_transfers_assets IF EXISTS USING btree (asset_id);
+CREATE INDEX IF NOT EXISTS atomicassets_transfers_assets_asset_id ON atomicassets_transfers_assets USING btree (asset_id);
--- definitions/tables/atomictools_migrate.sql
+++ definitions/tables/atomictools_migrate.sql
@@ -1,1 +1,1 @@
-CREATE INDEX IF NOT EXISTS atomictools_links_assets_asset_id ON atomictools_links_assets IF EXISTS USING btree (asset_id);
+CREATE INDEX IF NOT EXISTS atomictools_links_assets_asset_id ON atomictools_links_assets USING btree (asset_id);
