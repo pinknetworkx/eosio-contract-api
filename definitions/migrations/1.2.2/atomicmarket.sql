878e8dc71d32b47137f791d1a335dbfe4b15f6ae
Fabian Emilius <fabian.emilius@tum.de>
2021-05-04 16:59:23 +0200

    Only create indexes if not exist

--- definitions/migrations/1.2.2/atomicmarket.sql
+++ definitions/migrations/1.2.2/atomicmarket.sql
@@ -2,6 +2,6 @@ ALTER TABLE atomicmarket_sales ADD COLUMN IF NOT EXISTS template_mint int4range;
 ALTER TABLE atomicmarket_buyoffers ADD COLUMN IF NOT EXISTS template_mint int4range;
 ALTER TABLE atomicmarket_auctions ADD COLUMN IF NOT EXISTS template_mint int4range;
 
-CREATE INDEX atomicmarket_sales_missing_mint IF NOT EXISTS ON atomicmarket_sales(assets_contract, sale_id, offer_id) WHERE template_mint IS NULL;
-CREATE INDEX atomicmarket_buyoffers_missing_mint IF NOT EXISTS ON atomicmarket_buyoffers(assets_contract, buyoffer_id) WHERE template_mint IS NULL;
-CREATE INDEX atomicmarket_auctions_missing_mint IF NOT EXISTS ON atomicmarket_auctions(assets_contract, auction_id) WHERE template_mint IS NULL;
+CREATE INDEX IF NOT EXISTS atomicmarket_sales_missing_mint ON atomicmarket_sales(assets_contract, sale_id, offer_id) WHERE template_mint IS NULL;
+CREATE INDEX IF NOT EXISTS atomicmarket_buyoffers_missing_mint ON atomicmarket_buyoffers(assets_contract, buyoffer_id) WHERE template_mint IS NULL;
+CREATE INDEX IF NOT EXISTS atomicmarket_auctions_missing_mint ON atomicmarket_auctions(assets_contract, auction_id) WHERE template_mint IS NULL;
