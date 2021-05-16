18bec5ff4fc2dba5f7cfa06db7a030492acde37f
Fabian Emilius <fabian.emilius@tum.de>
2021-05-16 15:47:24 +0200

    Fix upgrade error

--- definitions/migrations/1.2.5/atomictools.sql
+++ definitions/migrations/1.2.5/atomictools.sql
@@ -2,4 +2,4 @@ DROP INDEX IF EXISTS atomictools_links_key_type;
 DROP INDEX IF EXISTS atomictools_links_key_data;
 DROP INDEX IF EXISTS atomictools_links_assets_tools_contract;
 
-CREATE INDEX atomictools_links_key_full IF NOT EXISTS ON atomictools_links USING hash (key_type, key_data);
+CREATE INDEX IF NOT EXISTS atomictools_links_key_full ON atomictools_links USING hash (key_type, key_data);
