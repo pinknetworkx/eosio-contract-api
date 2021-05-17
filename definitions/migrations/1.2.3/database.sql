244f2e4be2f3840e719b4d0254b1a90f58b2c543
Fabian Emilius <fabian.emilius@tum.de>
2021-05-17 22:45:22 +0200

    Fix contract trace upgrade

--- definitions/migrations/1.2.3/database.sql
+++ definitions/migrations/1.2.3/database.sql
@@ -1,5 +1,6 @@
 ALTER TABLE contract_traces DROP CONSTRAINT IF EXISTS atomicassets_logs_pkey;
 ALTER TABLE contract_traces DROP CONSTRAINT IF EXISTS contract_traces_pkey;
+ALTER TABLE contract_traces DROP CONSTRAINT IF EXISTS contract_traces_pkey2;
 ALTER TABLE contract_traces ADD CONSTRAINT contract_traces_pkey PRIMARY KEY (global_sequence, account);
 
 UPDATE dbinfo SET "value" = '1.2.3' WHERE name = 'version';
