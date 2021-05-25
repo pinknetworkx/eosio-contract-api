77370a3b82b286291dc03645311e1a7deb8a86c4
Fabian Emilius <fabian.emilius@tum.de>
2021-05-25 20:28:41 +0200

    Update transfer pkeys

--- definitions/migrations/1.2.6/atomicassets.sql
+++ definitions/migrations/1.2.6/atomicassets.sql
@@ -1,5 +1,10 @@
+ALTER TABLE atomicassets_transfers DROP CONSTRAINT IF EXISTS atomicassets_transfers_assets_transfers_fkey;
+
 ALTER TABLE atomicassets_transfers DROP CONSTRAINT IF EXISTS atomicassets_transfers_pkey;
 ALTER TABLE atomicassets_transfers ADD CONSTRAINT contract_traces_pkey PRIMARY KEY (contract, transfer_id);
 
 ALTER TABLE atomicassets_transfers_assets DROP CONSTRAINT IF EXISTS atomicassets_transfers_assets_pkey;
 ALTER TABLE atomicassets_transfers_assets ADD CONSTRAINT atomicassets_transfers_assets_pkey PRIMARY KEY (transfer_id, contract, asset_id);
+
+ALTER TABLE ONLY atomicassets_transfers_assets
+    ADD CONSTRAINT atomicassets_transfers_assets_transfers_fkey FOREIGN KEY (contract, transfer_id) REFERENCES atomicassets_transfers(contract, transfer_id) MATCH SIMPLE ON UPDATE RESTRICT ON DELETE RESTRICT DEFERRABLE INITIALLY DEFERRED NOT VALID;
