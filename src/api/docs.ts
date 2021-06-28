dc4f01b6115daf826d415ba82413ba29852c1cc8
Fabian Emilius <fabian.emilius@tum.de>
2021-06-28 18:27:00 +0200

    Change pink.network to pink.gg

--- src/api/docs.ts
+++ src/api/docs.ts
@@ -2,7 +2,7 @@ import { HTTPServer } from './server';
 
 export function getOpenApiDescription(server: HTTPServer): string {
     return '### EOSIO Contract API\n' +
-        '*Made with ♥️ by [pink.network](https://pink.network/)*\n' +
+        '*Made with ♥️ by [pink.gg](https://pink.gg/)*\n' +
         '#### Current Chain: ' + server.connection.chain.name + '\n' +
         `#### Provided by: [${server.config.provider_name}](${server.config.provider_url})`;
 }
