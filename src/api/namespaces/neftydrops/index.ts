a1e6cb0261c1541f21a289902572a09568bef70c
José Andrade <jandrad@gmail.com>
2021-09-02 10:37:42 +0200

    Fix docs endpoint.

--- src/api/namespaces/neftydrops/index.ts
+++ src/api/namespaces/neftydrops/index.ts
@@ -2,11 +2,8 @@ import * as express from 'express';
 
 import { ApiNamespace } from '../interfaces';
 import { HTTPServer } from '../../server';
-import { getOpenApiDescription } from '../../docs';
-import logger from '../../../utils/winston';
 import { neftyDropsComponents} from './openapi';
 import { configEndpoints } from './routes/config';
-import * as swagger from 'swagger-ui-express';
 import {statsEndpoints} from './routes/stats';
 import {dropsEndpoints} from './routes/drops';
 
@@ -47,53 +44,37 @@ export class NeftyDropsNamespace extends ApiNamespace {
     }
 
     async router(server: HTTPServer): Promise<express.Router> {
+
         const router = express.Router();
 
-        const documentation: any = {
-            openapi: '3.0.0',
-            info: {
-                description: getOpenApiDescription(server),
-                version: '1.0.0',
-                title: 'NeftyDrops API'
-            },
-            servers: [
-                {url: 'https://' + server.config.server_name + this.path},
-                {url: 'http://' + server.config.server_name + this.path}
-            ],
-            tags: [],
-            paths: {},
-            components: {
-                schemas: neftyDropsComponents
-            }
-        };
+        server.docs.addSchemas(neftyDropsComponents);
 
         if (server.web.limiter) {
             server.web.express.use(this.path + '/v1', server.web.limiter);
         }
 
-        const docs = [
-            configEndpoints(this, server, router),
-            dropsEndpoints(this, server, router),
-            statsEndpoints(this, server, router)
-        ];
+        const endpointsDocs = [];
+        endpointsDocs.push(configEndpoints(this, server, router));
+        endpointsDocs.push(dropsEndpoints(this, server, router));
+        endpointsDocs.push(statsEndpoints(this, server, router));
 
-        for (const doc of docs) {
-            Object.assign(documentation.paths, doc.paths);
-
+        for (const doc of endpointsDocs) {
             if (doc.tag) {
-                documentation.tags.push(doc.tag);
+                server.docs.addTags([doc.tag]);
+            }
+
+            if (doc.paths) {
+                const paths: any = {};
+
+                for (const path of Object.keys(doc.paths)) {
+                    paths[this.path + path] = doc.paths[path];
+                }
+
+                server.docs.addPaths(paths);
             }
         }
 
-        logger.info('neftydrops docs on ' + this.path + '/docs');
-        logger.debug('neftydrops swagger docs', documentation);
-
-        router.use('/docs', swagger.serve);
-        router.get('/docs', swagger.setup(documentation, {
-            customCss: '.topbar { display: none; }',
-            customCssUrl: 'https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-flattop.min.css'
-        }));
-
+        router.all(['/docs', '/docs/swagger'], (req, res) => res.redirect('/docs'));
         return router;
     }
 
