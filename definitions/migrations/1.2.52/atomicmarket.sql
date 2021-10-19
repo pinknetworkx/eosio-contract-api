60d891bc2bb8e7e2e34ebbef14df84b6afe35b8c
andreaneftyblocks <andrea@neftyblocks.com>
2021-10-19 10:49:43 +0200

    remove indexes from migration

--- definitions/migrations/1.2.52/atomicmarket.sql
+++ definitions/migrations/1.2.52/atomicmarket.sql
@@ -1,2 +1,2 @@
-CREATE INDEX IF NOT EXISTS atomicmarket_sales_taker_market ON atomicmarket_sales USING hash (taker_marketplace COLLATE pg_catalog."default");
-CREATE INDEX IF NOT EXISTS atomicmarket_sales_maker_market ON atomicmarket_sales USING hash (maker_marketplace COLLATE pg_catalog."default");
+-- CREATE INDEX IF NOT EXISTS atomicmarket_sales_taker_market ON atomicmarket_sales USING hash (taker_marketplace COLLATE pg_catalog."default");
+-- CREATE INDEX IF NOT EXISTS atomicmarket_sales_maker_market ON atomicmarket_sales USING hash (maker_marketplace COLLATE pg_catalog."default");
