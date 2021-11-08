c8a224205d99fb477dbdaa63d49cff0b9fe75e7f
Javier3448 <javier3448.ja@gmail.com>
2021-11-08 16:29:08 -0600

    Change query and add pagination

--- src/api/namespaces/atomicassets/routes/schemas.ts
+++ src/api/namespaces/atomicassets/routes/schemas.ts
@@ -115,44 +115,39 @@ export function schemasEndpoints(core: AtomicAssetsNamespace, server: HTTPServer
 
     router.all(['/v1/schemas/:collection_name/:schema_name/attribute_value_filters', '/v1/schemas/:collection_name/:schema_name/attribute_value_filters/_count'], server.web.caching({ignoreQueryString: true}), (async (req, res) => {
         try {
+            const args = filterQueryArgs(req, {
+                page: {type: 'int', min: 1, default: 1},
+                limit: {type: 'int', min: 1, max: 1000, default: 100},
+                sort: {type: 'string', values: ['key', 'value'], default: 'key'},
+                order: {type: 'string', values: ['asc', 'desc'], default: 'desc'},
+            });
+
             const query = new QueryBuilder(`
-                SELECT asset.asset_id, template.template_id, (asset.immutable_data || asset.mutable_data || template.immutable_data) as data
-                FROM atomicassets_assets asset
-                    LEFT JOIN atomicassets_templates template ON asset.template_id = template.template_id
-                WHERE asset.contract = $1 AND template.contract = $1 AND 
-                    asset.collection_name = $2 AND 
-                    asset.schema_name = $3
+                SELECT DISTINCT d.key, d.value
+                FROM
+                    atomicassets_templates as t, 
+                    jsonb_each(t.immutable_data) as d 
+                WHERE 
+                    t.contract = $1 AND
+                    t.collection_name = $2 AND
+                    t.schema_name = $3
+                ORDER BY
+                    d.${args.sort} ${args.order}
+                LIMIT $4
+                OFFSET $5
             `);
 
             const queryArgs = [
                 core.args.atomicassets_account, 
                 req.params.collection_name, 
-                req.params.schema_name
+                req.params.schema_name,
+                args.limit,
+                (args.page - 1) * args.limit
             ];
 
             const result = await server.query(query.buildString(), queryArgs);
 
-            let resData:any = {};
-            for(let row of result.rows){
-                for(let key in row.data){
-                    if(resData[key] === undefined){
-                        resData[key] = new Set();
-                    }
-
-                    resData[key].add(row.data[key].toString());
-                }
-            }
-
-            for(let key in resData){
-                if(resData[key].size > 100){
-                    resData[key] = undefined;
-                }
-                else{
-                    resData[key] = Array.from(resData[key]);
-                }
-            }
-
-            res.json({success: true, data: resData, query_time: Date.now()});
+            res.json({success: true, data: result.rows, query_time: Date.now()});
         } catch (e) {
             logger.error(e);
             res.status(500).json({success: false, message: 'Internal Server Error'});
