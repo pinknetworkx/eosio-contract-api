96c5a67c637b3507ac8c09c1108b52d10c62c614
José Andrade <jandrad@gmail.com>
2021-11-24 13:28:42 +0100

    Limit attribute filters to values below 100 characters and keys below 50 characters

--- definitions/views/neftydrops_attribute_filters_master.sql
+++ definitions/views/neftydrops_attribute_filters_master.sql
@@ -3,4 +3,5 @@ OR REPLACE VIEW neftydrops_attribute_filters_master AS
 SELECT DISTINCT t.contract, t.collection_name, t.schema_name, d.key, d.value
 FROM
     atomicassets_templates as t,
-    jsonb_each(t.immutable_data) as d;
+    jsonb_each(t.immutable_data) as d
+WHERE length(d.value::TEXT) < 100 AND length(d.key) < 50;
