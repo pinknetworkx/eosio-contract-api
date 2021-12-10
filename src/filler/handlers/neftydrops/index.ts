2a66ad3f30626511dec71523c2689e8617f0be96
José Andrade <jandrad@gmail.com>
2021-12-10 09:44:26 +0100

    Skip view refresh in 1.2.55 update.

--- src/filler/handlers/neftydrops/index.ts
+++ src/filler/handlers/neftydrops/index.ts
@@ -93,13 +93,6 @@ export default class NeftyDropsHandler extends ContractHandler {
                 await client.query(fs.readFileSync('./definitions/views/' + view + '.sql', {encoding: 'utf8'}));
             }
         }
-        if (version === '1.2.55') {
-            const viewsToUpdate = ['neftydrops_drops_master'];
-            for (const view of viewsToUpdate) {
-                logger.info(`Refreshing views ${view}`);
-                await client.query(fs.readFileSync('./definitions/views/' + view + '.sql', {encoding: 'utf8'}));
-            }
-        }
         if (version === '1.2.56') {
             const viewsToUpdate = ['neftydrops_drops_master'];
             for (const view of viewsToUpdate) {
