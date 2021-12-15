ef61d62d9ade03e2fb7b0dace72f9a799d261158
José Andrade <jandrad@gmail.com>
2021-12-15 17:13:48 +0100

    Fix filter default.

--- src/api/namespaces/utils.ts
+++ src/api/namespaces/utils.ts
@@ -32,7 +32,7 @@ export function filterQueryArgs(values: FilterValues, filter: FilterDefinition, 
             data = values[key]?.toString();
         }
 
-        if (filter[key].type === 'string' && typeof data !== 'string') {
+        if (typeof data !== 'string') {
             result[key] = filter[key].default;
 
             continue;
