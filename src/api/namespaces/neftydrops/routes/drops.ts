e870facbedb3c4c762bd74c0e01c5fcdcc2b3af1
José Andrade <jandrad@gmail.com>
2022-01-19 16:47:45 +0100

    Add ENDED state filter to drops api.

--- src/api/namespaces/neftydrops/routes/drops.ts
+++ src/api/namespaces/neftydrops/routes/drops.ts
@@ -43,6 +43,7 @@ export function dropsEndpoints(core: NeftyDropsNamespace, server: HTTPServer, ro
                                 DropApiState.ACTIVE.valueOf() + ': ACTIVE - The drop is active, ' +
                                 DropApiState.DELETED.valueOf() + ': DELETED - The drop is deleted' +
                                 DropApiState.SOLD_OUT.valueOf() + ': SOLD_OUT - The drop is sold out' +
+                                DropApiState.ENDED.valueOf() + ': SOLD_OUT - The drop is ended' +
                                 ') - separate multiple with ","',
                             required: false,
                             schema: {type: 'string'}
