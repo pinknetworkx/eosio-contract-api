3c951803f5339262b3bb386fabe377ca3d7843e5
Javier3448 <javier3448.ja@gmail.com>
2022-01-24 12:04:16 -0600

    Add template object to results in blend details endpoint

--- src/api/namespaces/neftyblends/handlers/blends.ts
+++ src/api/namespaces/neftyblends/handlers/blends.ts
@@ -127,10 +127,8 @@ export async function getBlendDetails(params: RequestValues, ctx: NeftyBlendsCon
 
     let ids = args.ids.split(',');
 
-    // @TODO @BIG-BUG: DON'T FORGET: THE CONTRACT IS A RELEVANT PART OF 
-    // THE PRIMARY KEY OF PRETTY MUCH ALL OF THE TABLES WE USE HERE!
     const query = new QueryBuilder(`
-    SELECT 
+SELECT 
 	blend.blend_id, 
 	blend.contract, 
 	blend.collection_name,
@@ -143,63 +141,9 @@ export async function getBlendDetails(params: RequestValues, ctx: NeftyBlendsCon
 			WHEN ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' THEN 'attributes'
 		END,
 		CASE
-			WHEN ingredient.ingredient_type = 'TEMPLATE_INGREDIENT' THEN
-				jsonb_build_object(
-					'contract', temp_ing_sub.contract,
-					'template_id', temp_ing_sub.template_id,
-					'transferable is_transferable', temp_ing_sub.transferable,
-					'burnable is_burnable', temp_ing_sub.burnable,
-					'issued_supply', temp_ing_sub.issued_supply,
-					'max_supply', temp_ing_sub.max_supply,
-					'collection_name', temp_ing_sub.collection_name,
-					'authorized_accounts', temp_ing_sub.collection_authorized_accounts,
-					'collection', jsonb_build_object(
-						'collection_name', temp_ing_sub.collection_collection_name,
-						'name', temp_ing_sub.collection_data->>'name',
-						'img', temp_ing_sub.collection_data->>'img',
-						'author', temp_ing_sub.collection_author,
-						'allow_notify', temp_ing_sub.collection_allow_notify,
-						'authorized_accounts', temp_ing_sub.collection_authorized_accounts,
-						'notify_accounts', temp_ing_sub.collection_notify_accounts,
-						'market_fee', temp_ing_sub.collection_market_fee,
-						'created_at_block', temp_ing_sub.collection_created_at_block::text,
-						'created_at_time', temp_ing_sub.collection_created_at_time::text
-					),
-					'schema_name', temp_ing_sub.schema_name,
-					'schema', jsonb_build_object(
-						'schema_name', temp_ing_sub.schema_schema_name,
-						'format', temp_ing_sub.schema_format,
-						'created_at_block', temp_ing_sub.schema_created_at_block::text,
-						'created_at_time', temp_ing_sub.schema_created_at_time::text
-					),
-					'immutable_data', temp_ing_sub.immutable_data,
-					'created_at_time', temp_ing_sub.created_at_time,
-					'created_at_block', temp_ing_sub.created_at_block
-				)
-		WHEN 
-			ingredient.ingredient_type = 'SCHEMA_INGREDIENT' THEN
-				jsonb_build_object(
-					'format', schema_ing_sub.collection_name, 
-					'schema_name', schema_ing_sub.schema_name, 
-					'collection_name', schema_ing_sub.format,
-					'collection', jsonb_build_object(
-						'collection_name', schema_ing_sub.collection_collection_name,
-						'name', schema_ing_sub.collection_data->>'name',
-						'img', schema_ing_sub.collection_data->>'img',
-						'author', schema_ing_sub.collection_author,
-						'allow_notify', schema_ing_sub.collection_allow_notify,
-						'authorized_accounts', schema_ing_sub.collection_authorized_accounts,
-						'notify_accounts', schema_ing_sub.collection_notify_accounts,
-						'market_fee', schema_ing_sub.collection_market_fee,
-						'created_at_block', schema_ing_sub.collection_created_at_block::text,
-						'created_at_time', schema_ing_sub.collection_created_at_time::text
-					),
-					'created_at_time', schema_ing_sub.created_at_time, 
-					'created_at_block', schema_ing_sub.created_at_block
-				)
-		WHEN 
-			ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' THEN
-					attribute_ing_sub.attributes
+			WHEN ingredient.ingredient_type = 'TEMPLATE_INGREDIENT' THEN temp_ing_sub.template_json_object
+			WHEN ingredient.ingredient_type = 'SCHEMA_INGREDIENT' THEN schema_ing_sub.schema_json_object
+			WHEN ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' THEN attribute_ing_sub.attributes
 		END
 	)) as ingredients,
 	jsonb_agg(jsonb_build_object(
@@ -210,33 +154,43 @@ FROM
 	neftyblends_blends blend
 	JOIN 
 		neftyblends_blend_ingredients "ingredient" ON
+		ingredient.contract = blend.contract AND
 		ingredient.blend_id = blend.blend_id
 	LEFT JOIN (
 		SELECT
-			template.contract as contract,
-			template_id as template_id,
-			transferable as transferable,
-			burnable as burnable,
-			issued_supply as issued_supply,
-			max_supply as max_supply,
-			"template".collection_name as collection_name,
-			collection.collection_name as collection_collection_name,
-			collection.data as collection_data,
-			collection.author as collection_author,
-			collection.allow_notify as collection_allow_notify,
-			collection.authorized_accounts as collection_authorized_accounts,
-			collection.notify_accounts as collection_notify_accounts,
-			collection.market_fee as collection_market_fee,
-			collection.created_at_block as collection_created_at_block,
-			collection.created_at_time as collection_created_at_time,
-			"template".schema_name as schema_name,
-			"schema".schema_name as schema_schema_name,
-			"schema".format as schema_format,
-			"schema".created_at_block as schema_created_at_block,
-			"schema".created_at_time as schema_created_at_time,
-			immutable_data as immutable_data,
-			"template".created_at_time as created_at_time,
-			"template".created_at_block as created_at_block
+			"template".template_id as template_id,
+			jsonb_build_object(
+				'contract', "template".contract,
+				'template_id', "template".template_id,
+				'transferable is_transferable', "template".transferable,
+				'burnable is_burnable', "template".burnable,
+				'issued_supply', "template".issued_supply,
+				'max_supply', "template".max_supply,
+				'collection_name', "template".collection_name,
+				'authorized_accounts', collection.authorized_accounts,
+				'collection', jsonb_build_object(
+					'collection_name', collection.collection_name,
+					'name', collection.data->>'name',
+					'img', collection.data->>'img',
+					'author', collection.author,
+					'allow_notify', collection.allow_notify,
+					'authorized_accounts', collection.authorized_accounts,
+					'notify_accounts', collection.notify_accounts,
+					'market_fee', collection.market_fee,
+					'created_at_block', collection.created_at_block::text,
+					'created_at_time', collection.created_at_time::text
+				),
+				'schema_name', "template".schema_name,
+				'schema', jsonb_build_object(
+					'schema_name', "schema".schema_name,
+					'format', "schema".format,
+					'created_at_block', "schema".created_at_block::text,
+					'created_at_time', "schema".created_at_time::text
+				),
+				'immutable_data', "template".immutable_data,
+				'created_at_time', "template".created_at_time,
+				'created_at_block', "template".created_at_block
+			) as template_json_object
 		FROM
 			atomicassets_templates "template" 
 			JOIN 
@@ -253,18 +207,24 @@ FROM
 		SELECT
 			"schema".collection_name as collection_name,
 			"schema".schema_name as schema_name,
-			"schema".format as format,
-			collection.collection_name as collection_collection_name,
-			collection.data as collection_data,
-			collection.author as collection_author,
-			collection.allow_notify as collection_allow_notify,
-			collection.authorized_accounts as collection_authorized_accounts,
-			collection.notify_accounts as collection_notify_accounts,
-			collection.market_fee as collection_market_fee,
-			collection.created_at_block as collection_created_at_block,
-			collection.created_at_time as collection_created_at_time,
-			"schema".created_at_time as created_at_time,
-			"schema".created_at_block as created_at_block
+			jsonb_build_object(
+				'collection_name', collection.collection_name,
+				'format', "schema".format, 
+				'schema_name', "schema".schema_name, 
+				'collection', jsonb_build_object(
+					'collection_name', collection.collection_name,
+					'name', collection.data->>'name',
+					'img', collection.data->>'img',
+					'author', collection.author,
+					'allow_notify', collection.allow_notify,
+					'authorized_accounts', collection.authorized_accounts,
+					'notify_accounts', collection.notify_accounts,
+					'market_fee', collection.market_fee,
+					'created_at_block', collection.created_at_block::text,
+					'created_at_time', collection.created_at_time::text
+				),
+				'created_at_time', "schema".created_at_time, 
+				'created_at_block', "schema".created_at_block) as schema_json_object
 		FROM atomicassets_schemas "schema"
 			JOIN 
 				atomicassets_collections collection ON
@@ -275,6 +235,7 @@ FROM
 		schema_ing_sub.schema_name = ingredient.schema_name
 	LEFT JOIN(
 		SELECT 
+			ing_attribute.contract,
 			ing_attribute.blend_id,
 			ing_attribute.ingredient_index,
 			jsonb_agg(jsonb_build_object(
@@ -284,59 +245,108 @@ FROM
 		FROM
 			neftyblends_blend_ingredient_attributes ing_attribute
 		GROUP BY
-			ing_attribute.blend_id, ing_attribute.ingredient_index
+			ing_attribute.contract, ing_attribute.blend_id, ing_attribute.ingredient_index
 	) as attribute_ing_sub ON
 		ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' AND 
+		attribute_ing_sub.contract = ingredient.contract AND
 		attribute_ing_sub.blend_id = ingredient.blend_id AND
 		attribute_ing_sub.ingredient_index = ingredient.ingredient_index
-LEFT JOIN(
-	SELECT 
-		roll.blend_id,
-		roll.roll_index,
-		roll.total_odds as total_odds,
-		jsonb_agg(jsonb_build_object(
-			'odds', outcome_sub.odds,
-			'results', outcome_sub.results
-		)) as outcomes
-	FROM
-		neftyblends_blend_rolls roll
-	LEFT JOIN (
-		SELECT 
-			outcome.contract,
-			outcome.blend_id,
-			outcome.roll_index,
-			outcome.outcome_index,
-			outcome.odds,
-			jsonb_agg(jsonb_build_object(
-				-- @TODO? maybe add a property with the result type as well
-				case when "result"."type" = 'POOL_NFT_RESULT' then 'pool'
-				when "result"."type" = 'ON_DEMAND_NFT_RESULT' then 'template'
-				end,
-				-- @TODO: if ON_DEMAND_NFT_RESULT we have to build the template object
-				-- just like we did with ingredients
-				"result".payload
-			)) as results
-		FROM
-			neftyblends_blend_roll_outcomes as outcome
-			JOIN neftyblends_blend_roll_outcome_results as "result" ON
-				outcome.contract = "result".contract AND
-				outcome.blend_id = "result".blend_id AND
-				outcome.roll_index = "result".roll_index AND
-				outcome.outcome_index = "result".outcome_index
-		GROUP BY
-			outcome.contract,
-			outcome.blend_id,
-			outcome.roll_index,
-			outcome.outcome_index,
-			outcome.odds
-	) as outcome_sub ON
-		outcome_sub.contract = roll.contract AND
-		outcome_sub.blend_id = roll.blend_id AND
-		outcome_sub.roll_index = roll.roll_index
-	group by
-		roll.blend_id, roll.roll_index, roll.total_odds
-) as roll_sub ON
-	roll_sub.blend_id = blend.blend_id
+    LEFT JOIN(
+        SELECT 
+            roll.contract,
+            roll.blend_id,
+            roll.roll_index,
+            roll.total_odds as total_odds,
+            jsonb_agg(jsonb_build_object(
+                'odds', outcome_sub.odds,
+                'results', outcome_sub.results
+            )) as outcomes
+        FROM
+            neftyblends_blend_rolls roll
+        LEFT JOIN (
+            SELECT 
+                outcome.contract,
+                outcome.blend_id,
+                outcome.roll_index,
+                outcome.outcome_index,
+                outcome.odds,
+                jsonb_agg(jsonb_build_object(
+                    case when "result"."type" = 'POOL_NFT_RESULT' then 'pool'
+                    when "result"."type" = 'ON_DEMAND_NFT_RESULT' then 'template'
+                    end,
+                    case when "result"."type" = 'POOL_NFT_RESULT' then "result".payload
+                    when "result"."type" = 'ON_DEMAND_NFT_RESULT' then result_template_sub.template_json_object
+                    end
+                )) as results
+            FROM
+                neftyblends_blend_roll_outcomes as outcome
+                JOIN neftyblends_blend_roll_outcome_results as "result" ON
+                    outcome.contract = "result".contract AND
+                    outcome.blend_id = "result".blend_id AND
+                    outcome.roll_index = "result".roll_index AND
+                    outcome.outcome_index = "result".outcome_index
+                LEFT JOIN (
+                    SELECT
+                        "template".template_id as template_id,
+                        jsonb_build_object(
+                            'contract', "template".contract,
+                            'template_id', "template".template_id,
+                            'transferable is_transferable', "template".transferable,
+                            'burnable is_burnable', "template".burnable,
+                            'issued_supply', "template".issued_supply,
+                            'max_supply', "template".max_supply,
+                            'collection_name', "template".collection_name,
+                            'authorized_accounts', collection.authorized_accounts,
+                            'collection', jsonb_build_object(
+                                'collection_name', collection.collection_name,
+                                'name', collection.data->>'name',
+                                'img', collection.data->>'img',
+                                'author', collection.author,
+                                'allow_notify', collection.allow_notify,
+                                'authorized_accounts', collection.authorized_accounts,
+                                'notify_accounts', collection.notify_accounts,
+                                'market_fee', collection.market_fee,
+                                'created_at_block', collection.created_at_block::text,
+                                'created_at_time', collection.created_at_time::text
+                            ),
+                            'schema_name', "template".schema_name,
+                            'schema', jsonb_build_object(
+                                'schema_name', "schema".schema_name,
+                                'format', "schema".format,
+                                'created_at_block', "schema".created_at_block::text,
+                                'created_at_time', "schema".created_at_time::text
+                            ),
+                            'immutable_data', "template".immutable_data,
+                            'created_at_time', "template".created_at_time,
+                            'created_at_block', "template".created_at_block
+                        ) as template_json_object
+                    FROM
+                        atomicassets_templates "template" 
+                        JOIN 
+                            atomicassets_collections collection ON
+                                "template".collection_name = collection.collection_name
+                        JOIN 
+                            atomicassets_schemas "schema" ON
+                                "template".collection_name = "schema".collection_name AND
+                                "template".schema_name = "schema".schema_name
+                ) as result_template_sub ON
+                    "result"."type" = 'ON_DEMAND_NFT_RESULT' AND
+                    cast ("result".payload->>'template_id' as bigint) = result_template_sub.template_id
+            GROUP BY
+                outcome.contract,
+                outcome.blend_id,
+                outcome.roll_index,
+                outcome.outcome_index,
+                outcome.odds
+        ) as outcome_sub ON
+            outcome_sub.contract = roll.contract AND
+            outcome_sub.blend_id = roll.blend_id AND
+            outcome_sub.roll_index = roll.roll_index
+        group by
+            roll.contract, roll.blend_id, roll.roll_index, roll.total_odds
+    ) as roll_sub ON
+        roll_sub.contract = blend.contract AND
+        roll_sub.blend_id = blend.blend_id
             `);
     query.equalMany('blend.blend_id', ids);
     query.append(`
