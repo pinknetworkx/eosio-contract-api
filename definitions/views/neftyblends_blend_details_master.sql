e627f226b960a6905879683368d996f234ae1675
Javier3448 <javier3448.ja@gmail.com>
2022-01-26 14:59:10 -0600

    Add param defaults and use sql params instead of appending to query string

--- definitions/views/neftyblends_blend_details_master.sql
+++ definitions/views/neftyblends_blend_details_master.sql
@@ -1,57 +1,64 @@
 CREATE OR REPLACE VIEW neftyblends_blend_details_master AS
 SELECT 
-	blend.blend_id, 
-	blend.contract, 
-	blend.collection_name,
- 	jsonb_agg(jsonb_build_object(
-		'ingredient_type', ingredient.ingredient_type,
-		'ingredient_amount', ingredient.amount,
-		CASE
-			WHEN ingredient.ingredient_type = 'TEMPLATE_INGREDIENT' THEN 'template'
-			WHEN ingredient.ingredient_type = 'SCHEMA_INGREDIENT' THEN 'schema'
-			WHEN ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' THEN 'attributes'
-		END,
-		CASE
-			WHEN ingredient.ingredient_type = 'TEMPLATE_INGREDIENT' THEN temp_ing_sub.template_json_object
-			WHEN ingredient.ingredient_type = 'SCHEMA_INGREDIENT' THEN schema_ing_sub.schema_json_object
-			WHEN ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' THEN attribute_ing_sub.attributes
-		END
-	)) as ingredients,
-	jsonb_agg(jsonb_build_object(
-		'total_odds', roll_sub.total_odds,
-		'outcomes', roll_sub.outcomes
-	)) as rolls
+    blend.blend_id, 
+    blend.contract, 
+    blend.collection_name,
+    blend.start_time,
+    blend.end_time,
+    blend.max,
+    blend.use_count,
+    blend.display_data,
+    blend.created_at_time,
+    blend.ingredients_count,
+    jsonb_agg(jsonb_build_object(
+        'ingredient_type', ingredient.ingredient_type,
+        'ingredient_amount', ingredient.amount,
+        CASE
+            WHEN ingredient.ingredient_type = 'TEMPLATE_INGREDIENT' THEN 'template'
+            WHEN ingredient.ingredient_type = 'SCHEMA_INGREDIENT' THEN 'schema'
+            WHEN ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' THEN 'attributes'
+        END,
+        CASE
+            WHEN ingredient.ingredient_type = 'TEMPLATE_INGREDIENT' THEN temp_ing_sub.template_json_object
+            WHEN ingredient.ingredient_type = 'SCHEMA_INGREDIENT' THEN schema_ing_sub.schema_json_object
+            WHEN ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' THEN attribute_ing_sub.attributes
+        END
+    )) as ingredients,
+    jsonb_agg(jsonb_build_object(
+        'total_odds', roll_sub.total_odds,
+        'outcomes', roll_sub.outcomes
+    )) as rolls
 FROM 
-	neftyblends_blends blend
-	JOIN 
-		neftyblends_blend_ingredients "ingredient" ON
-		ingredient.contract = blend.contract AND
-		ingredient.blend_id = blend.blend_id
-	LEFT JOIN neftyblends_template_details_master as temp_ing_sub ON
-		ingredient.ingredient_type = 'TEMPLATE_INGREDIENT' AND 
-		temp_ing_sub.template_id = ingredient.template_id
-	LEFT JOIN neftyblends_schema_details_master as schema_ing_sub ON
-		ingredient.ingredient_type = 'SCHEMA_INGREDIENT' AND 
-		schema_ing_sub.collection_name = ingredient.ingredient_collection_name AND
-		schema_ing_sub.schema_name = ingredient.schema_name
-	LEFT JOIN(
-		SELECT 
-			ing_attribute.contract,
-			ing_attribute.blend_id,
-			ing_attribute.ingredient_index,
-			jsonb_agg(jsonb_build_object(
-				'name', ing_attribute.attribute_name,
-				'allowed_values', ing_attribute.allowed_values
-			)) as "attributes"
-		FROM
-			neftyblends_blend_ingredient_attributes ing_attribute
-		GROUP BY
-			ing_attribute.contract, ing_attribute.blend_id, ing_attribute.ingredient_index
-	) as attribute_ing_sub ON
-		ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' AND 
-		attribute_ing_sub.contract = ingredient.contract AND
-		attribute_ing_sub.blend_id = ingredient.blend_id AND
-		attribute_ing_sub.ingredient_index = ingredient.ingredient_index
+    neftyblends_blends blend
+    JOIN 
+        neftyblends_blend_ingredients "ingredient" ON
+        ingredient.contract = blend.contract AND
+        ingredient.blend_id = blend.blend_id
+    LEFT JOIN neftyblends_template_details_master as temp_ing_sub ON
+        ingredient.ingredient_type = 'TEMPLATE_INGREDIENT' AND 
+        temp_ing_sub.template_id = ingredient.template_id
+    LEFT JOIN neftyblends_schema_details_master as schema_ing_sub ON
+        ingredient.ingredient_type = 'SCHEMA_INGREDIENT' AND 
+        schema_ing_sub.collection_name = ingredient.ingredient_collection_name AND
+        schema_ing_sub.schema_name = ingredient.schema_name
+    LEFT JOIN(
+        SELECT 
+            ing_attribute.contract,
+            ing_attribute.blend_id,
+            ing_attribute.ingredient_index,
+            jsonb_agg(jsonb_build_object(
+                'name', ing_attribute.attribute_name,
+                'allowed_values', ing_attribute.allowed_values
+            )) as "attributes"
+        FROM
+            neftyblends_blend_ingredient_attributes ing_attribute
+        GROUP BY
+            ing_attribute.contract, ing_attribute.blend_id, ing_attribute.ingredient_index
+    ) as attribute_ing_sub ON
+        ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' AND 
+        attribute_ing_sub.contract = ingredient.contract AND
+        attribute_ing_sub.blend_id = ingredient.blend_id AND
+        attribute_ing_sub.ingredient_index = ingredient.ingredient_index
     LEFT JOIN(
         SELECT 
             roll.contract,
@@ -105,6 +112,13 @@ FROM
         roll_sub.contract = blend.contract AND
         roll_sub.blend_id = blend.blend_id
 GROUP BY
-	blend.blend_id, 
-	blend.contract, 
-	blend.collection_name;
+    blend.blend_id, 
+    blend.contract, 
+    blend.collection_name,
+    blend.start_time,
+    blend.end_time,
+    blend.max,
+    blend.use_count,
+    blend.display_data,
+    blend.created_at_time,
+    blend.ingredients_count;
--- src/api/namespaces/neftyblends/handlers/blends.ts
+++ src/api/namespaces/neftyblends/handlers/blends.ts
@@ -6,117 +6,170 @@ import { ApiError } from '../../../error';
 
 export async function getIngredientOwnershipBlendFilter(params: RequestValues, ctx: NeftyBlendsContext): Promise<any> {
     const args = filterQueryArgs(params, {
-        contract: {type: 'string', values: ['blend.nefty', 'blenderizerx'], default: ""},
+        contract: {type: 'string', default: ""},
         collection_name: {type: 'string', default: ""},
-        ingredient_owner_id: {type: 'string', default: ""},
-        owned_ingredients_amount: {type: 'string', values: ['all', 'one'], default: ""},
-        order_by: {type: 'string', values: ['blend_id', 'creation_date'], default: ""},
-        must_be_available: {type: 'string', values: ['true', 'false'], default: ""},
+        ingredient_owner: {type: 'string', default: ""},
+        ingredient_match: {type: 'string', values: ['all', 'any'], default: "any"},
+        sort: {type: 'string', values: ['blend_id', 'created_at_time'], default: "blend_id"},
+        order: {type: 'string', values: ['asc', 'desc'], default: "desc"},
+        available_only: {type: 'bool', default: false},
     });
 
-    // @TODO: have a different error message for when the query param is not missing 
-    // it has an invalid arg value
-    if(args.contract === ""){
-        throw new ApiError("Missing or invalid required query parameter: contract", 400);
-    }
-    if(args.collection_name === ""){
-        throw new ApiError("Missing or invalid required query parameter: collection_name", 400);
-    }
-    if(args.ingredient_owner_id === ""){
-        throw new ApiError("Missing or invalid required query parameter: ingredient_owner_id", 400);
-    }
-    if(args.owned_ingredients_amount === ""){
-        throw new ApiError( "Missing or invalid required query parameter: owned_ingredients_amount", 400);
-    }
-    if(args.order_by === ""){
-        throw new ApiError( "Missing or invalid required query parameter: order_by", 400);
-    }
-    if(args.must_be_available === ""){
-        throw new ApiError( "Missing or invalid required query parameter: must_be_available", 400);
-    }
+    // @TODO: dont use default value if arg value is not in `values`, throw error 
+    // instead
 
-    let amountToMatch;
-    if(args.owned_ingredients_amount === "all"){
-        amountToMatch = 'sub.ingredients_count';
-    }
-    else{
-        amountToMatch = '1';
-    }
-    let must_be_available_condition;
-    if(args.must_be_available === "true"){
-        let nowEpoch = Date.now();
-
-        must_be_available_condition = `AND (
-            (b.start_time = 0 OR ${nowEpoch} >= b.start_time) AND
-            (b.end_time = 0 OR ${nowEpoch} <= b.end_time) AND
-            (b.max = 0 OR b.max > b.use_count)
-        )
-        `
-    }
-    else{
-        must_be_available_condition = '';
-    }
-
-    // @TODO: Remove query comments
-    // @TODO: use the QueryBuilder (if possible) (or at least use the query args
-	//        which I think helps avoid sql injection)
     // @TODO: add openapi spec
-    // @TODO: return relevant blend info, not just the blend_id
-    // @TODO: If we don't have a good constant `order by` the distinct on might
+    // @TODO: If we don't have a good constant `order by` the `distinct on` might
     //        return "unexpected results"
-    // @TODO: Add the sql code to create the new function this query uses
-    const query = new QueryBuilder(`
-		SELECT 
-			blend_detail.*
-		FROM
-			(
-				SELECT 
-					sub.contract, 
-					sub.blend_id, 
-					sub.ingredients_count,
-					sub.created_at_time as creation_date,
-					count(1) ingredient_requirement_fulfilled
-				FROM(
-					-- The 'DISTINCT ON' ensures that the same asset_id is not "matched" twice in the same blend 
-					SELECT DISTINCT ON(b.blend_id, a.asset_id) 
-						b.contract, 
-						b.blend_id, 
-						a.asset_id, 
-						b.ingredients_count,
-						b.created_at_time 
-					FROM
-						neftyblends_blends b 
-						JOIN neftyblends_blend_ingredients i ON
-							b.blend_id = i.blend_id
-						JOIN atomicassets_assets a ON 
-							(i.ingredient_type = 'TEMPLATE_INGREDIENT' AND a.template_id = i.template_id) OR
-							(i.ingredient_type = 'SCHEMA_INGREDIENT' AND a.schema_name = i.schema_name) OR
-							(i.ingredient_type = 'ATTRIBUTE_INGREDIENT' AND is_ingredient_attribute_match(a.template_id, b.blend_id, i.ingredient_index, i.total_attributes))
-					WHERE
-						-- Assets the owner owns 
-						a.collection_name = '${args.collection_name}' AND 
-						a.owner = '${args.ingredient_owner_id}' AND
-						-- blends in collection 
-						b.collection_name = '${args.collection_name}' AND
-						-- which contract
-						b.contract = '${args.contract}'
-						${must_be_available_condition}
-				) as sub
-				group by 
-					sub.contract, 
-					sub.blend_id, 
-					sub.ingredients_count, 
-					sub.created_at_time 
-				HAVING 
-					count(1) >= ${amountToMatch}
-				ORDER BY ${args.order_by}
-			) as blend_filter_sub 
-		JOIN neftyblends_blend_details_master as blend_detail ON
-			blend_filter_sub.contract = blend_detail.contract AND
-			blend_filter_sub.blend_id = blend_detail.blend_id;
-    `);
 
-    const result = await ctx.db.query(query.buildString(), query.buildValues());
+    let queryVarCounter:number = 0;
+    let queryValues:any[] = [];
+    let queryString:string;
+    // If we don't have to figure out if the owner has the assets required to
+    // execute the blend the query is a lot simpler, basically just blend_details
+    // view
+    if(args.ingredient_owner === ""){
+        queryString = `
+            SELECT 
+                blend_detail.*
+            FROM
+                neftyblends_blend_details_master blend_detail
+            WHERE
+        `;
+
+        // bodge so we can always append `WHERE` to the string, even if no conditions
+        // were sent in `args`. 
+        queryString += `
+                TRUE`
+        ;
+        if(args.collection_name !== ""){
+            queryValues.push(args.collection_name);
+            queryString += `
+                AND blend_detail.collection_name = $${++queryVarCounter}`
+            ;
+        }
+        if(args.contract !== ""){
+            queryValues.push(args.contract);
+            queryString += `
+                AND blend_detail.contract = $${++queryVarCounter}`
+            ;
+        }
+        if(args.available_only){
+            queryString += `
+                AND (
+                    (blend_detail.start_time = 0 OR (cast(extract(epoch from now()) as bigint) * 1000) >= blend_detail.start_time) AND
+                    (blend_detail.end_time = 0 OR (cast(extract(epoch from now()) as bigint) * 1000) <= blend_detail.end_time) AND
+                    (blend_detail.max = 0 OR blend_detail.max > blend_detail.use_count)
+                )`
+            ;
+        }
+    }
+    else{
+        queryString=`
+        SELECT 
+            blend_detail.*
+        FROM
+        (
+            SELECT 
+                asset_matches_sub.contract, 
+                asset_matches_sub.blend_id, 
+                asset_matches_sub.ingredients_count,
+                count(1) ingredient_requirement_fulfilled
+            FROM(` +
+                // The `DISTINCT ON` ensures that the same asset_id is not "matched" twice in the same blend 
+    `            SELECT DISTINCT ON(b.blend_id, a.asset_id) 
+                    b.contract, 
+                    b.blend_id, 
+                    a.asset_id, 
+                    b.ingredients_count,
+                    b.created_at_time 
+                FROM
+                    neftyblends_blends b 
+                    JOIN neftyblends_blend_ingredients i ON
+                        b.blend_id = i.blend_id
+                    JOIN atomicassets_assets a ON 
+                        (i.ingredient_type = 'TEMPLATE_INGREDIENT' AND a.template_id = i.template_id) OR
+                        (i.ingredient_type = 'SCHEMA_INGREDIENT' AND a.schema_name = i.schema_name) OR
+                        (i.ingredient_type = 'ATTRIBUTE_INGREDIENT' AND is_ingredient_attribute_match(a.template_id, b.blend_id, i.ingredient_index, i.total_attributes))
+                WHERE`
+            ;
+        // add `WHERE` conditions:
+        {
+            queryValues.push(args.ingredient_owner);
+            queryString += `
+                    a.owner = $${++queryVarCounter}`
+            ;
+            if(args.collection_name !== ""){
+                // Out of the assets the user owns, we only care about the ones 
+                // belonging to this collection. This assumes that a blends can 
+                // only have ingredients that are from the same collection
+                // This saves an absurd amount of time in the query! 
+                // ~(30 secs to 300ms, for the `asset_matches_sub` subquery!)
+                queryValues.push(args.collection_name);
+                queryString += `
+                    AND a.collection_name = $${++queryVarCounter}`
+                ;
+
+                // blends in collection
+                queryValues.push(args.collection_name);
+                queryString += `
+                    AND b.collection_name = $${++queryVarCounter}`
+                ;
+            }
+            if(args.contract !== ""){
+                queryValues.push(args.contract);
+                queryString += `
+                    AND b.contract = $${++queryVarCounter}`
+                ;
+            }
+            if(args.available_only){
+                queryString += `
+                    AND (
+                        (b.start_time = 0 OR (cast(extract(epoch from now()) as bigint) * 1000) >= b.start_time) AND
+                        (b.end_time = 0 OR (cast(extract(epoch from now()) as bigint) * 1000) <= b.end_time) AND
+                        (b.max = 0 OR b.max > b.use_count)
+                    )`
+                ;
+            }
+        }
+
+        queryString += `
+            ) as asset_matches_sub
+            group by 
+                asset_matches_sub.contract, 
+                asset_matches_sub.blend_id,
+                asset_matches_sub.ingredients_count
+            HAVING 
+        `;
+        if(args.ingredient_match === 'all'){
+            queryString += `
+                count(1) >= asset_matches_sub.ingredients_count
+            `;
+        }
+        else{
+            queryString += `
+                count(1) >= 1
+            `;
+        }
+
+        queryString += `
+        ) as blend_filter_sub 
+        JOIN neftyblends_blend_details_master as blend_detail ON
+            blend_filter_sub.contract = blend_detail.contract AND
+            blend_filter_sub.blend_id = blend_detail.blend_id
+        `;
+    }
+
+    // This should not lead to sql injection as long as `filterQueryArgs` enforces
+    // the allowed values in `sort` and `order`
+    queryString += `
+    ORDER BY
+        blend_detail.${args.sort}
+    ${args.order};
+    `;
+
+
+    const result = await ctx.db.query(queryString, queryValues);
 
     return result.rows;
 }
@@ -124,7 +177,7 @@ export async function getIngredientOwnershipBlendFilter(params: RequestValues, c
 export async function getBlendDetails(params: RequestValues, ctx: NeftyBlendsContext): Promise<any> {
 
     const query = new QueryBuilder(`
-		SELECT *  FROM neftyblends_blend_details_master blend_detail
+        SELECT *  FROM neftyblends_blend_details_master blend_detail
     `);
     query.equal('blend_detail.blend_id', ctx.pathParams.blend_id);
     query.equal('blend_detail.contract', ctx.pathParams.contract);
