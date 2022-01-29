031a710abed4c15be94286938d2b9f8a0ea7a29e
Javier3448 <javier3448.ja@gmail.com>
2022-01-28 19:33:19 -0600

    Add openapi documentation to blends endpoints

--- src/api/namespaces/neftyblends/openapi.ts
+++ src/api/namespaces/neftyblends/openapi.ts
@@ -1,12 +1,54 @@
 
 export const neftyBlendsComponents = {
     // @TODO:
-    'Blend': {
+    'BlendDetails': {
         type: 'object',
-        properties: {}
-    },
-    'BriefBlend': {
-        type: 'object',
-        properties: {}
+        properties: {
+            blend_id: {type: 'string'},
+            contract: {type: 'string'},
+            collection_name: {type: 'string'},
+            start_time: {type: 'string'},
+            end_time: {type: 'string'},
+            max: {type: 'string'},
+            use_count: {type: 'string'},
+            display_data: {type: 'string'},
+            created_at_time: {type: 'string'},
+            ingredients_count: {type: 'string'},
+            ingredients: {
+                type: 'array',
+                // @TODO:
+                // There is actually 3 different possible structures, 
+                // template_ingredient, attribute_ingredient and schema_ingredient
+                // but I dont know how to do unions here. For now `any` is good enough
+                items: {type: 'object'}
+            },
+            rolls: {
+                type: 'array',
+                items: {
+                    type: 'object',
+                    properties: {
+                        total_odds: {type: 'number'},
+                        outcomes:{
+                            type: 'array',
+                            items: {
+                                type: 'object',
+                                properties: {
+                                    odds: {type: 'number'},
+                                    results: {
+                                        type: 'array',
+                                        // @TODO:
+                                        // There is actually 2 different possible 
+                                        // structures, template_result, pool_nft_result
+                                        // but I dont know how to do unions here. 
+                                        // For now `any` is good enough
+                                        items: {type: 'object'}
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
     }
 };
--- src/api/namespaces/neftyblends/routes/blends.ts
+++ src/api/namespaces/neftyblends/routes/blends.ts
@@ -26,30 +26,53 @@ export function filtersEndpoints(core: NeftyBlendsNamespace, server: HTTPServer,
             '/v1/ingredient_ownership_blend_filter': {
                 get: {
                     tags: ['neftyblends'],
-                    summary: 'Get unique (attribute_name, atribute_value) pairs',
+                    summary: 'Get blends that a given collector has ingredients to',
                     description: 
-                        'Get every unique (attribute_name, atribute_value) pairs' + 
-                        'in all the templates of a schema',
+                        'Given a collection and an ingredient_owner, returns all ' +
+                        'the blends that both: are in that collection and that the ' + 
+                        'ingredient_owner owns any or all ingredients to',
                     parameters: [
                         {
-                            name: 'collection_name',
-                            in: 'path',
-                            description: 'Collection name of schema',
-                            required: true,
+                            name: 'contract',
+                            in: 'query',
+                            description: 'Blend contract of blends (nefty.blend or blenderizerx)',
+                            required: false,
                             schema: {type: 'string'}
                         },
                         {
-                            name: 'schema_name',
-                            in: 'path',
-                            description: 'Name of schema',
-                            required: true,
+                            name: 'collection_name',
+                            in: 'query',
+                            description: 'Collection name of blends',
+                            required: false,
                             schema: {type: 'string'}
                         },
+                        {
+                            name: 'ingredient_owner',
+                            in: 'query',
+                            description: 'User that owns the ingredients that will be tested against each',
+                            required: false,
+                            schema: {type: 'string'}
+                        },
+                        {
+                            name: 'ingredient_match',
+                            in: 'query',
+                            description: 'How many ingredients should be matched in each blend (all or any)',
+                            required: false,
+                            schema: {type: 'string'}
+                        },
+                        {
+                            name: 'available_only',
+                            in: 'query',
+                            description: 'If true, it filters out all the blends that haven\'t started or have already ended',
+                            required: false,
+                            schema: {type: 'string'}
+                        },
+
                         ...paginationParameters
                     ],
                     responses: getOpenAPI3Responses([200, 500], {
                         type: 'array',
-                        items: {'$ref': '#/components/schemas/AttributeValueFilter'}
+                        items: {'$ref': '#/components/schemas/BlendDetails'}
                     })
                 }
             },
@@ -72,34 +95,28 @@ export function blendDetailsEndpoints(core: NeftyBlendsNamespace, server: HTTPSe
             description: 'NeftyBlends'
         },
         paths: {
-            '/v1/ingredient_ownership_blend_filter': {
+            '/v1/blends/{contract}/{blend_id}': {
                 get: {
                     tags: ['neftyblends'],
-                    summary: 'Get unique (attribute_name, atribute_value) pairs',
-                    description: 
-                        'Get every unique (attribute_name, atribute_value) pairs' + 
-                        'in all the templates of a schema',
+                    summary: 'Get blend details',
+                    description: 'Get details of a single blend',
                     parameters: [
                         {
-                            name: 'collection_name',
+                            name: 'contract',
                             in: 'path',
-                            description: 'Collection name of schema',
+                            description: 'Blend contract',
                             required: true,
                             schema: {type: 'string'}
                         },
                         {
-                            name: 'schema_name',
+                            name: 'blend_id',
                             in: 'path',
-                            description: 'Name of schema',
+                            description: 'Blend id',
                             required: true,
                             schema: {type: 'string'}
-                        },
-                        ...paginationParameters
+                        }
                     ],
-                    responses: getOpenAPI3Responses([200, 500], {
-                        type: 'array',
-                        items: {'$ref': '#/components/schemas/AttributeValueFilter'}
-                    })
+                    responses: getOpenAPI3Responses([200, 500], { '$ref': '#/components/schemas/BlendDetails' })
                 }
             },
         }
