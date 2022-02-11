283a616c2947063b6e243ccc12c3c260343e53cd
Andre Meyer <amozaq@gmail.com>
2022-02-11 12:14:20 -0800

    Fix version

--- definitions/migrations/1.3.6/atomicassets.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX atomicassets_templates_name ON atomicassets_templates USING GIST ((immutable_data->>'name') gist_trgm_ops);
-CREATE INDEX atomicassets_collections_name ON atomicassets_collections USING GIST ((collection_name || ' ' || COALESCE(data->>'name', '')) gist_trgm_ops);
--- definitions/migrations/1.3.6/database.sql
+++
@@ -1,1 +0,0 @@
-UPDATE dbinfo SET "value" = '1.3.6' WHERE name = 'version';
