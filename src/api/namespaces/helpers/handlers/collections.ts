13f81931be1d85006ff407de6f4e30137c12c4f8
Javier3448 <javier3448.ja@gmail.com>
2022-02-27 23:02:18 -0600

    Add simple cache to helpers/colletions endpoint

--- src/api/namespaces/helpers/handlers/collections.ts
+++ src/api/namespaces/helpers/handlers/collections.ts
@@ -3,7 +3,29 @@ import {NeftyMarketContext} from '../index';
 import QueryBuilder from '../../../builder';
 import { ApiError } from '../../../error';
 
+const updateRate = 60 * 1000;
+type Cache = {
+  lastUpdate: Date;
+  rows: any[];
+};
+let cache:Cache = {
+    lastUpdate: new Date(0),
+    rows: null
+}
+
 export async function getCollectionsAction(params: RequestValues, ctx: NeftyMarketContext): Promise<any> {
+    if (
+        cache.rows === null || 
+        (Date.now() > (cache.lastUpdate.getTime() + updateRate))
+    ) {
+        cache.lastUpdate = new Date(Date.now());
+        cache.rows = await getCollectionsLists(ctx);
+    }
+
+    return cache;
+}
+
+async function getCollectionsLists(ctx: NeftyMarketContext):Promise<any[]>{
     const query = new QueryBuilder(`
         SELECT collection_name, jsonb_agg(jsonb_build_object('contract', contract, 'list_name', list)) as lists
         FROM helpers_collection_list as v
