cde437bbf4a60fe75fbdfea0f0b4a11ed4072f23
Fabian Emilius <fabian.emilius@tum.de>
2022-04-11 09:40:44 +0200

    Add aurora optimization options

---
+++ definitions/migrations/1.3.11/atomicassets.sql
@@ -0,0 +1,4 @@
+ALTER TABLE atomicassets_templates SET (autovacuum_vacuum_scale_factor = 0.0);
+ALTER TABLE atomicassets_templates SET (autovacuum_vacuum_threshold = 100);
+ALTER TABLE atomicassets_templates SET (autovacuum_analyze_scale_factor = 0.0);
+ALTER TABLE atomicassets_templates SET (autovacuum_analyze_threshold = 1000);
