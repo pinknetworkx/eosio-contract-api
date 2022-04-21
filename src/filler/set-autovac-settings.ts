1e3609c9b035fa1d2c5a4853ea6a8a2b419a937d
Andre Meyer <amozaq@gmail.com>
2022-04-21 08:36:37 -0700

    Update tables individually to prevent deadlocks

--- src/filler/set-autovac-settings.ts
+++ src/filler/set-autovac-settings.ts
@@ -10,7 +10,7 @@ const settingOverrides: {[key: string]: {scale: string, threshold: number}} = {
 export async function setAutoVacSettings(database: PostgresConnection): Promise<void> {
     const {rows} = await database.query('SELECT schemaname, relname AS tablename, n_live_tup::INT AS rows FROM pg_stat_user_tables');
 
-    const sql = rows.map(table => {
+    for (const table of rows) {
         let scale = '0.05';
         let threshold = 50;
 
@@ -30,7 +30,7 @@ export async function setAutoVacSettings(database: PostgresConnection): Promise<
             threshold = settingOverrides[table.tablename].threshold;
         }
 
-        return `
+        await database.query(`
             ALTER TABLE ${table.schemaname}.${table.tablename} SET (
                 autovacuum_vacuum_scale_factor = ${scale},
                 autovacuum_vacuum_threshold = ${threshold},
@@ -38,9 +38,8 @@ export async function setAutoVacSettings(database: PostgresConnection): Promise<
                 autovacuum_analyze_threshold = ${threshold * 10},
                 autovacuum_vacuum_insert_scale_factor = ${scale},
                 autovacuum_vacuum_insert_threshold = ${threshold * 10}
-            );
-        `;
-    }).join('\n');
+            )
+        `);
+    }
 
-    await database.query(sql);
 }
