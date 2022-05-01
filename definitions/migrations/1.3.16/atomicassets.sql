82a9b57657c1bf4d13d350ec3d1fd96c0c0f7206
José Andrade <jandrad@gmail.com>
2022-05-01 14:12:39 +0200

    Add collection && schema index to atomicassets_assets

--- definitions/migrations/1.3.16/atomicassets.sql
+++ definitions/migrations/1.3.16/atomicassets.sql
@@ -1,3 +1,5 @@
+CREATE INDEX IF NOT EXISTS atomicassets_assets_collection_name_schema_name on atomicassets_assets (collection_name, "schema_name");
+
 SELECT brin_summarize_new_values('atomicassets_mints_idx_asset_id'::regclass);
 
 ALTER INDEX atomicassets_mints_idx_asset_id set (autosummarize = on);
