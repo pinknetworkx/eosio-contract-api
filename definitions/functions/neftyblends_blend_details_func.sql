448da049cbc9d4457da6c99bf47d7a5e92ba7a36
Javier3448 <javier3448.ja@gmail.com>
2022-05-10 15:52:41 -0600

    Add ingredient_typed_attributes and upgrade_specs to blend_details

--- definitions/functions/neftyblends_blend_details_func.sql
+++ definitions/functions/neftyblends_blend_details_func.sql
@@ -1,3 +1,8 @@
+-- this function does the same as the view 'neftyblends_blend_details_master'
+-- we created it only because we noticed a performace improvement when calling
+-- `neftyblends_blend_details_func('collection_name')` instead of 
+-- `select * from neftyblends_blend_details_master 
+--  where collection_name = 'collection_name` since we can use
 DROP FUNCTION IF EXISTS neftyblends_blend_details_func(collection_name character varying(13));
 CREATE FUNCTION neftyblends_blend_details_func(collection_name character varying(13))
   RETURNS TABLE (blend_id bigint, contract character varying(12), collection_name character varying(13), start_time bigint, end_time bigint, max bigint, use_count bigint, display_data text, created_at_time bigint, ingredients_count integer, security_id bigint, is_hidden boolean, ingredients jsonb, rolls jsonb, category character varying(255))
@@ -28,6 +33,7 @@ $body$
                     WHEN ingredient.ingredient_type = 'SCHEMA_INGREDIENT' THEN 'schema'
                     WHEN ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' THEN 'attributes'
                     WHEN ingredient.ingredient_type = 'BALANCE_INGREDIENT' THEN 'template'
+                    WHEN ingredient.ingredient_type = 'TYPED_ATTRIBUTE_INGREDIENT' THEN 'typed_attributes'
                     END,
                 CASE
                     WHEN ingredient.ingredient_type = 'TEMPLATE_INGREDIENT' THEN jsonb_build_object(
@@ -46,13 +52,24 @@ $body$
                             'template_id', ingredient.template_id,
                             'schema_name', ingredient.schema_name
                         )
+                    WHEN ingredient.ingredient_type = 'TYPED_ATTRIBUTE_INGREDIENT' THEN jsonb_build_object(
+                            'typed_attributes', typed_attribute_ing_sub.typed_attributes,
+                            'schema_name', ingredient.schema_name,
+                            'collection_name', ingredient.ingredient_collection_name
+                        )
                     END
-            )) as ingredients,
+            )) FILTER (where ingredient.ingredient_index is not null) as ingredients,
         jsonb_agg(DISTINCT jsonb_build_object(
                 'index', roll_sub.roll_index,
                 'total_odds', roll_sub.total_odds,
                 'outcomes', roll_sub.outcomes
-            )) as rolls,
+            )) FILTER (where roll_sub.roll_index is not null) as rolls,
+        jsonb_agg(jsonb_build_object(
+            'schema_name', upg_spec_sub.schema_name,
+            'display_data', upg_spec_sub.display_data,
+            'upgrade_requirements', upg_spec_sub.upgrade_requirements,
+            'upgrade_results', upg_spec_sub.upgrade_results)
+        ) FILTER (where upg_spec_sub.upgrade_spec_index is not null) as upgrade_specs,
         blend.category
     FROM
         neftyblends_blends blend
@@ -78,6 +95,25 @@ $body$
                     attribute_ing_sub.blend_id = ingredient.blend_id AND
                     attribute_ing_sub.ingredient_index = ingredient.ingredient_index
             LEFT JOIN(
+            SELECT
+                ing_typed_attribute.contract,
+                ing_typed_attribute.blend_id,
+                ing_typed_attribute.ingredient_index,
+                jsonb_agg(jsonb_build_object(
+                        'name', ing_typed_attribute.attribute_name,
+                        'type', ing_typed_attribute.attribute_type,
+                        'allowed_values', ing_typed_attribute.allowed_values
+                    )) as "typed_attributes"
+            FROM
+                neftyblends_blend_ingredient_typed_attributes ing_typed_attribute
+            GROUP BY
+                ing_typed_attribute.contract, ing_typed_attribute.blend_id, ing_typed_attribute.ingredient_index
+        ) as typed_attribute_ing_sub ON
+            ingredient.ingredient_type = 'TYPED_ATTRIBUTE_INGREDIENT' AND
+            typed_attribute_ing_sub.contract = ingredient.contract AND
+            typed_attribute_ing_sub.blend_id = ingredient.blend_id AND
+            typed_attribute_ing_sub.ingredient_index = ingredient.ingredient_index
+        LEFT JOIN(
             SELECT
                 roll.contract,
                 roll.blend_id,
@@ -119,8 +155,97 @@ $body$
         ) as roll_sub ON
                     roll_sub.contract = blend.contract AND
                     roll_sub.blend_id = blend.blend_id
+        LEFT JOIN(
+            SELECT
+                upg_spec.contract,
+                upg_spec.blend_id,
+                upg_spec.upgrade_spec_index,
+                upg_spec.schema_name,
+                upg_spec.display_data,
+                jsonb_agg(jsonb_build_object(
+                    'type', upg_req_sub.type,
+                    CASE
+                        WHEN upg_req_sub."type" = 'TEMPLATE_REQUIREMENT' THEN 'template_requirement'
+                        WHEN upg_req_sub."type" = 'TYPED_ATTRIBUTE_REQUIREMENT' THEN 'typed_attribute_requirement'
+                    END,
+                    CASE
+                        WHEN upg_req_sub."type" = 'TEMPLATE_REQUIREMENT' THEN
+                            jsonb_build_object(
+                                'template_id', upg_req_sub.template_id
+                            )
+                        WHEN upg_req_sub."type" = 'TYPED_ATTRIBUTE_REQUIREMENT' THEN
+                            upg_req_sub.typed_attribute_definition
+                    END
+                )) FILTER (where upg_req_sub.upgrade_requirement_index is not null) as upgrade_requirements, -- could have a typed_attributes subquery like ingredients
+                jsonb_agg(jsonb_build_object(
+                    'attribute_name', upg_res_sub.attribute_name,
+                    'attribute_type', upg_res_sub.attribute_type,
+                    'upgrade_operator', upg_res_sub.upgrade_operator,
+                    'result_value_type', upg_res_sub.result_value_type,
+                    'result_value',
+                    CASE
+                        WHEN upg_res_sub.result_value_type = 'VALUE_ROLL_RESULT' THEN
+                            jsonb_build_object(
+                                -- @TODO?,  do the join with valueroll table
+                                'valueroll_id', upg_res_sub.valueroll_id
+                            )
+                        WHEN upg_res_sub.result_value_type = 'IMMEDIATE_VALUE' AND upg_res_sub.immediate_type = 'string' THEN
+                            jsonb_build_object(
+                                'immediate_type', upg_res_sub.immediate_type,
+                                'immediate_result', upg_res_sub.immediate_string
+                            )
+                        WHEN upg_res_sub.result_value_type = 'IMMEDIATE_VALUE' AND upg_res_sub.immediate_type = 'uint64' THEN
+                            jsonb_build_object(
+                                'immediate_type', upg_res_sub.immediate_type,
+                                'immediate_result', upg_res_sub.immediate_uint64
+                            )
+                    END
+                )) FILTER (where upg_res_sub.upgrade_result_index is not null) as upgrade_results
+            FROM
+                neftyblends_blend_upgrade_specs upg_spec
+                LEFT JOIN (
+                    SELECT
+                        upg_req.contract,
+                        upg_req.blend_id,
+                        upg_req.upgrade_spec_index,
+                        upg_req.upgrade_requirement_index,
+                        upg_req.type,
+                        upg_req.template_id,
+                        upg_req.typed_attribute_definition
+                    FROM
+                        neftyblends_blend_upgrade_spec_upgrade_requirements upg_req
+                ) AS upg_req_sub ON
+                    upg_req_sub.contract = upg_spec.contract AND
+                    upg_req_sub.blend_id = upg_spec.blend_id AND
+                    upg_req_sub.upgrade_spec_index = upg_spec.upgrade_spec_index
+                LEFT JOIN (
+                    SELECT
+                        upg_res.contract,
+                        upg_res.blend_id,
+                        upg_res.upgrade_spec_index,
+                        upg_res.upgrade_result_index,
+                        upg_res.attribute_name,
+                        upg_res.attribute_type,
+                        upg_res.upgrade_operator,
+                        upg_res.blend_collection_name,
+                        upg_res.result_value_type,
+                        upg_res.immediate_type,
+                        upg_res.valueroll_id,
+                        upg_res.immediate_string,
+                        upg_res.immediate_uint64
+                    FROM
+                        neftyblends_blend_upgrade_spec_upgrade_results upg_res
+                ) AS upg_res_sub ON
+                    upg_res_sub.contract = upg_spec.contract AND
+                    upg_res_sub.blend_id = upg_spec.blend_id AND
+                    upg_res_sub.upgrade_spec_index = upg_spec.upgrade_spec_index
+            GROUP BY
+                upg_spec.contract, upg_spec.blend_id, upg_spec.upgrade_spec_index, upg_spec.schema_name, upg_spec.display_data
+        ) as upg_spec_sub ON
+            upg_spec_sub.contract = blend.contract AND
+            upg_spec_sub.blend_id = blend.blend_id
     WHERE
-        blend.collection_name = $1
+        blend.contract = $1
     GROUP BY
         blend.blend_id,
         blend.contract,
