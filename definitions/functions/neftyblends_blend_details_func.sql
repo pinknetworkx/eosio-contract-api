42514116a0f4ec798be4b0c15152a733d8be349e
Javier3448 <javier3448.ja@gmail.com>
2022-05-10 16:17:53 -0600

    Fix formatting in blend_details queries

--- definitions/functions/neftyblends_blend_details_func.sql
+++ definitions/functions/neftyblends_blend_details_func.sql
@@ -23,92 +23,97 @@ $body$
         blend.security_id,
         blend.is_hidden,
         jsonb_agg(DISTINCT jsonb_build_object(
-                'type', ingredient.ingredient_type,
-                'effect', ingredient.effect,
-                'amount', ingredient.amount,
-                'index', ingredient.ingredient_index,
-                'display_data', ingredient.display_data,
-                CASE
-                    WHEN ingredient.ingredient_type = 'TEMPLATE_INGREDIENT' THEN 'template'
-                    WHEN ingredient.ingredient_type = 'SCHEMA_INGREDIENT' THEN 'schema'
-                    WHEN ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' THEN 'attributes'
-                    WHEN ingredient.ingredient_type = 'BALANCE_INGREDIENT' THEN 'template'
-                    WHEN ingredient.ingredient_type = 'TYPED_ATTRIBUTE_INGREDIENT' THEN 'typed_attributes'
-                    END,
-                CASE
-                    WHEN ingredient.ingredient_type = 'TEMPLATE_INGREDIENT' THEN jsonb_build_object(
-                            'template_id', ingredient.template_id
-                        )
-                    WHEN ingredient.ingredient_type = 'SCHEMA_INGREDIENT' THEN jsonb_build_object(
-                            'schema_name', ingredient.schema_name,
-                            'collection_name', ingredient.ingredient_collection_name
-                        )
-                    WHEN ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' THEN jsonb_build_object(
-                            'attributes', attribute_ing_sub.attributes,
-                            'schema_name', ingredient.schema_name,
-                            'collection_name', ingredient.ingredient_collection_name
-                        )
-                    WHEN ingredient.ingredient_type = 'BALANCE_INGREDIENT' THEN jsonb_build_object(
-                            'template_id', ingredient.template_id,
-                            'schema_name', ingredient.schema_name
-                        )
-                    WHEN ingredient.ingredient_type = 'TYPED_ATTRIBUTE_INGREDIENT' THEN jsonb_build_object(
-                            'typed_attributes', typed_attribute_ing_sub.typed_attributes,
-                            'schema_name', ingredient.schema_name,
-                            'collection_name', ingredient.ingredient_collection_name
-                        )
-                    END
-            )) FILTER (where ingredient.ingredient_index is not null) as ingredients,
+            'type', ingredient.ingredient_type,
+            'effect', ingredient.effect,
+            'amount', ingredient.amount,
+            'index', ingredient.ingredient_index,
+            'display_data', ingredient.display_data,
+            CASE
+                WHEN ingredient.ingredient_type = 'TEMPLATE_INGREDIENT' THEN 'template'
+                WHEN ingredient.ingredient_type = 'SCHEMA_INGREDIENT' THEN 'schema'
+                WHEN ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' THEN 'attributes'
+                WHEN ingredient.ingredient_type = 'BALANCE_INGREDIENT' THEN 'template'
+                WHEN ingredient.ingredient_type = 'TYPED_ATTRIBUTE_INGREDIENT' THEN 'typed_attributes'
+            END,
+            CASE
+                WHEN ingredient.ingredient_type = 'TEMPLATE_INGREDIENT' THEN 
+                    jsonb_build_object(
+                        'template_id', ingredient.template_id
+                    )
+                WHEN ingredient.ingredient_type = 'SCHEMA_INGREDIENT' THEN 
+                    jsonb_build_object(
+                        'schema_name', ingredient.schema_name,
+                        'collection_name', ingredient.ingredient_collection_name
+                    )
+                WHEN ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' THEN
+                    jsonb_build_object(
+                        'attributes', attribute_ing_sub.attributes,
+                        'schema_name', ingredient.schema_name,
+                        'collection_name', ingredient.ingredient_collection_name
+                    )
+                WHEN ingredient.ingredient_type = 'BALANCE_INGREDIENT' THEN
+                    jsonb_build_object(
+                        'template_id', ingredient.template_id,
+                        'schema_name', ingredient.schema_name
+                    )
+                WHEN ingredient.ingredient_type = 'TYPED_ATTRIBUTE_INGREDIENT' THEN
+                    jsonb_build_object(
+                        'typed_attributes', typed_attribute_ing_sub.typed_attributes,
+                        'schema_name', ingredient.schema_name,
+                        'collection_name', ingredient.ingredient_collection_name
+                    )
+            END
+        )) FILTER (where ingredient.ingredient_index is not null) as ingredients,
         jsonb_agg(DISTINCT jsonb_build_object(
-                'index', roll_sub.roll_index,
-                'total_odds', roll_sub.total_odds,
-                'outcomes', roll_sub.outcomes
-            )) FILTER (where roll_sub.roll_index is not null) as rolls,
+            'index', roll_sub.roll_index,
+            'total_odds', roll_sub.total_odds,
+            'outcomes', roll_sub.outcomes
+        )) FILTER (where roll_sub.roll_index is not null) as rolls,
         jsonb_agg(jsonb_build_object(
             'schema_name', upg_spec_sub.schema_name,
             'display_data', upg_spec_sub.display_data,
             'upgrade_requirements', upg_spec_sub.upgrade_requirements,
-            'upgrade_results', upg_spec_sub.upgrade_results)
-        ) FILTER (where upg_spec_sub.upgrade_spec_index is not null) as upgrade_specs,
+            'upgrade_results', upg_spec_sub.upgrade_results
+        )) FILTER (where upg_spec_sub.upgrade_spec_index is not null) as upgrade_specs,
         blend.category
     FROM
         neftyblends_blends blend
-            JOIN neftyblends_blend_ingredients "ingredient" ON
-                    ingredient.contract = blend.contract
-                AND ingredient.blend_id = blend.blend_id
-            LEFT JOIN(
-                SELECT
-                    ing_attribute.contract,
-                    ing_attribute.blend_id,
-                    ing_attribute.ingredient_index,
-                    jsonb_agg(jsonb_build_object(
-                            'name', ing_attribute.attribute_name,
-                            'allowed_values', ing_attribute.allowed_values
-                        )) as "attributes"
-                FROM
-                    neftyblends_blend_ingredient_attributes ing_attribute
-                GROUP BY
-                    ing_attribute.contract, ing_attribute.blend_id, ing_attribute.ingredient_index
-            ) as attribute_ing_sub ON
-                    ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' AND
-                    attribute_ing_sub.contract = ingredient.contract AND
-                    attribute_ing_sub.blend_id = ingredient.blend_id AND
-                    attribute_ing_sub.ingredient_index = ingredient.ingredient_index
-            LEFT JOIN(
+        LEFT JOIN neftyblends_blend_ingredients "ingredient" ON
+            ingredient.contract = blend.contract AND
+            ingredient.blend_id = blend.blend_id
+        LEFT JOIN(
+            SELECT
+                ing_attribute.contract,
+                ing_attribute.blend_id,
+                ing_attribute.ingredient_index,
+                jsonb_agg(jsonb_build_object(
+                    'name', ing_attribute.attribute_name,
+                    'allowed_values', ing_attribute.allowed_values
+                )) as "attributes"
+            FROM
+                neftyblends_blend_ingredient_attributes ing_attribute
+            GROUP BY
+                ing_attribute.contract, ing_attribute.blend_id, ing_attribute.ingredient_index
+        ) as attribute_ing_sub ON
+            ingredient.ingredient_type = 'ATTRIBUTE_INGREDIENT' AND
+            attribute_ing_sub.contract = ingredient.contract AND
+            attribute_ing_sub.blend_id = ingredient.blend_id AND
+            attribute_ing_sub.ingredient_index = ingredient.ingredient_index
+        LEFT JOIN(
             SELECT
                 ing_typed_attribute.contract,
                 ing_typed_attribute.blend_id,
                 ing_typed_attribute.ingredient_index,
                 jsonb_agg(jsonb_build_object(
-                        'name', ing_typed_attribute.attribute_name,
-                        'type', ing_typed_attribute.attribute_type,
-                        'allowed_values', ing_typed_attribute.allowed_values
-                    )) as "typed_attributes"
+                    'name', ing_typed_attribute.attribute_name,
+                    'type', ing_typed_attribute.attribute_type,
+                    'allowed_values', ing_typed_attribute.allowed_values
+                )) as "typed_attributes"
             FROM
                 neftyblends_blend_ingredient_typed_attributes ing_typed_attribute
             GROUP BY
                 ing_typed_attribute.contract, ing_typed_attribute.blend_id, ing_typed_attribute.ingredient_index
-        ) as typed_attribute_ing_sub ON
+        ) AS typed_attribute_ing_sub ON
             ingredient.ingredient_type = 'TYPED_ATTRIBUTE_INGREDIENT' AND
             typed_attribute_ing_sub.contract = ingredient.contract AND
             typed_attribute_ing_sub.blend_id = ingredient.blend_id AND
@@ -120,12 +125,12 @@ $body$
                 roll.roll_index,
                 roll.total_odds as total_odds,
                 jsonb_agg(jsonb_build_object(
-                        'odds', outcome_sub.odds,
-                        'results', outcome_sub.results
-                    )) as outcomes
+                    'odds', outcome_sub.odds,
+                    'results', outcome_sub.results
+                )) as outcomes
             FROM
                 neftyblends_blend_rolls roll
-                    LEFT JOIN (
+                LEFT JOIN (
                     SELECT
                         outcome.contract,
                         outcome.blend_id,
@@ -135,11 +140,11 @@ $body$
                         COALESCE(jsonb_agg("result") filter (where "type" IS NOT NULL), '[]'::jsonb) as results
                     FROM
                         neftyblends_blend_roll_outcomes as outcome
-                            LEFT JOIN neftyblends_blend_roll_outcome_results as "result" ON
-                                    outcome.contract = "result".contract AND
-                                    outcome.blend_id = "result".blend_id AND
-                                    outcome.roll_index = "result".roll_index AND
-                                    outcome.outcome_index = "result".outcome_index
+                        LEFT JOIN neftyblends_blend_roll_outcome_results as "result" ON
+                            outcome.contract = "result".contract AND
+                            outcome.blend_id = "result".blend_id AND
+                            outcome.roll_index = "result".roll_index AND
+                            outcome.outcome_index = "result".outcome_index
                     GROUP BY
                         outcome.contract,
                         outcome.blend_id,
@@ -147,14 +152,14 @@ $body$
                         outcome.outcome_index,
                         outcome.odds
                 ) as outcome_sub ON
-                            outcome_sub.contract = roll.contract AND
-                            outcome_sub.blend_id = roll.blend_id AND
-                            outcome_sub.roll_index = roll.roll_index
-            group by
+                    outcome_sub.contract = roll.contract AND
+                    outcome_sub.blend_id = roll.blend_id AND
+                    outcome_sub.roll_index = roll.roll_index
+            GROUP BY
                 roll.contract, roll.blend_id, roll.roll_index, roll.total_odds
-        ) as roll_sub ON
-                    roll_sub.contract = blend.contract AND
-                    roll_sub.blend_id = blend.blend_id
+        ) AS roll_sub ON
+            roll_sub.contract = blend.contract AND
+            roll_sub.blend_id = blend.blend_id
         LEFT JOIN(
             SELECT
                 upg_spec.contract,
