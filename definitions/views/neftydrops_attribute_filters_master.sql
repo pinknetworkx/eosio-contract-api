742283fa420b6a2ba8d145379ded0c6c51cbccfc
José Andrade <jandrad@gmail.com>
2022-05-27 06:25:40 -0600

    Remove unused ugrades

--- definitions/views/neftydrops_attribute_filters_master.sql
+++ definitions/views/neftydrops_attribute_filters_master.sql
@@ -4,4 +4,4 @@ SELECT DISTINCT t.contract, t.collection_name, t.schema_name, d.key, d.value
 FROM
     atomicassets_templates as t,
     jsonb_each(t.immutable_data) as d
-WHERE length(d.value::TEXT) < 100 AND length(d.key) < 50;
+WHERE length(d.value::TEXT) <= 50 AND length(d.key) <= 50;
