3bfc64b92d52a5818bd068927150be0f44796081
Andre Meyer <amozaq@gmail.com>
2022-05-28 13:13:00 -0700

    Faster bulk refresh

--- definitions/migrations/1.3.14/atomicmarket.sql
+++ definitions/migrations/1.3.14/atomicmarket.sql
@@ -34,62 +34,55 @@ DECLARE
     result INT = 0;
     temp INT;
     rec RECORD;
-    _suggested_median BIGINT;
-    _suggested_average BIGINT;
     current_block_time BIGINT = (SELECT MAX(block_time) FROM contract_readers);
 BEGIN
     FOR rec IN
-        SELECT DISTINCT template_id, assets_contract
-        FROM atomicmarket_stats_prices_master
-        WHERE template_id IS NOT NULL
+        WITH templates AS MATERIALIZED (
+            SELECT DISTINCT template_id, assets_contract
+            FROM atomicmarket_stats_prices_master
+            WHERE template_id IS NOT NULL
+        ), sales AS MATERIALIZED (
+            SELECT assets_contract, SUBSTRING(f FROM 2)::BIGINT template_id, MIN(price) min_price
+            FROM atomicmarket_sales_filters_listed
+                JOIN LATERAL UNNEST(filter) u(f) ON u.f LIKE 't%'
+            WHERE seller_contract IS DISTINCT FROM TRUE
+                AND asset_count = 1
+        	    AND updated_at_time + 0 <= (current_block_time - 3600 * 24 * 3 * 1000) -- only include sales older than 3 days
+            GROUP BY template_id, assets_contract
+        )
+        SELECT template_id, assets_contract, sug.suggested_median, sug.suggested_average
+        FROM templates
+            LEFT OUTER JOIN sales USING (template_id, assets_contract)
+            CROSS JOIN LATERAL (
+                SELECT
+                    LEAST(PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY price), sales.min_price) suggested_median,
+                    LEAST(AVG(price)::BIGINT, sales.min_price) suggested_average
+                FROM (
+                        (
+                            SELECT listing_id /* not used, but required to prevent the same price being discarded in the union*/, price
+                            FROM atomicmarket_stats_prices_master
+                            WHERE template_id = templates.template_id AND assets_contract = templates.assets_contract
+                                AND time >= ((extract(epoch from now() - '3 days'::INTERVAL)) * 1000)::BIGINT
+                        )
+                        UNION
+                        (
+                            SELECT listing_id, price
+                            FROM atomicmarket_stats_prices_master
+                            WHERE template_id = templates.template_id AND assets_contract = templates.assets_contract
+                            ORDER BY time DESC
+                            LIMIT 5
+                        )
+                    ) prices
+            ) sug
 	LOOP
-		SELECT
-			PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY price) suggested_median,
-			AVG(price)::BIGINT suggested_average
-		INTO _suggested_median, _suggested_average
-		FROM (
-		        (
-                    SELECT listing_id /* not used, but required to prevent the same price being discarded in the union*/, price
-                    FROM atomicmarket_stats_prices_master
-                    WHERE template_id = rec.template_id AND assets_contract = rec.assets_contract
-                        AND time >= ((extract(epoch from now() - '3 days'::INTERVAL)) * 1000)::BIGINT
-				)
-				UNION
-				(
-                    SELECT listing_id, price
-                    FROM atomicmarket_stats_prices_master
-                    WHERE template_id = rec.template_id AND assets_contract = rec.assets_contract
-                    ORDER BY time DESC
-                    LIMIT 5
-				)
-			) prices
-		;
-
-        -- '+ 0' to prevent wrong index usage
-        SELECT LEAST(MIN(price + 0), _suggested_median), LEAST(MIN(price + 0), _suggested_average)
-		INTO _suggested_median, _suggested_average
-		FROM atomicmarket_sales_filters_listed
-		WHERE filter && create_atomicmarket_sales_filter(ARRAY[rec.template_id])
-		    AND assets_contract = rec.assets_contract
-		    AND seller_contract IS DISTINCT FROM TRUE
-		    AND updated_at_time + 0 <= (current_block_time - 3600 * 24 * 3 * 1000); -- older than 3 days
-
-		IF (_suggested_median IS NULL)
-		THEN
-			DELETE FROM atomicmarket_template_prices
-			WHERE template_id = rec.template_id AND assets_contract = rec.assets_contract;
-			result = result + 1;
-			CONTINUE;
-		END IF;
-
 		INSERT INTO atomicmarket_template_prices AS tp (market_contract, assets_contract, collection_name, template_id, symbol,
     			median, average, suggested_median, suggested_average, "min", "max", sales)
 			SELECT
 				market_contract, assets_contract, collection_name, template_id, symbol,
 				PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY price) median,
 				AVG(price)::bigint average,
-				_suggested_median,
-				_suggested_average,
+				rec.suggested_median,
+				rec.suggested_average,
 				MIN(price) "min", MAX(price) "max", COUNT(*) sales
 			FROM atomicmarket_stats_prices_master
 			WHERE template_id = rec.template_id AND assets_contract = rec.assets_contract
@@ -115,6 +108,18 @@ BEGIN
 		result = result + temp;
 	END LOOP;
 
+    IF (random() <= 0.05) -- occasionally, remove deleted templates
+    THEN
+        DELETE FROM atomicmarket_template_prices
+        WHERE (template_id, assets_contract) NOT IN (
+            SELECT DISTINCT template_id, assets_contract
+            FROM atomicmarket_stats_prices_master
+            WHERE template_id IS NOT NULL
+        );
+        GET DIAGNOSTICS temp = ROW_COUNT;
+		result = result + temp;
+    END IF;
+
     RETURN result;
 END
 $$;
@@ -123,5 +128,4 @@ SELECT update_atomicmarket_template_prices();
 
 create index atomicmarket_template_prices_collection_name on atomicmarket_template_prices (collection_name);
 create index atomicmarket_template_prices_template_id on atomicmarket_template_prices (template_id);
-
 
