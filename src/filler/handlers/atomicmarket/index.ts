5e7315434004124886441911a34e301352848ce0
José Andrade <jandrad@gmail.com>
2022-06-16 14:14:36 +0200

    Fix update script for atomicmarket

--- src/filler/handlers/atomicmarket/index.ts
+++ src/filler/handlers/atomicmarket/index.ts
@@ -137,7 +137,7 @@ export default class AtomicMarketHandler extends ContractHandler {
             }
         }
 
-        if (version === '1.3.13') {
+        if (version === '1.3.19') {
             await client.query(fs.readFileSync('./definitions/views/atomicmarket_stats_prices_master.sql', {encoding: 'utf8'}));
             await client.query(fs.readFileSync('./definitions/views/atomicmarket_template_prices_master.sql', {encoding: 'utf8'}));
         }
