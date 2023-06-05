0c536e02c5e41ad373fa636c486359964de973c1
Fabian Emilius <fabian.emilius@tum.de>
2023-06-05 16:55:23 +0200

    Fix linting error

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -18,7 +18,7 @@ export function arrayChunk(arr: any[], size: number): any[] {
     );
 }
 
-export function arrayUnique<T>(array: T[], compare: (a: T, b: T) => boolean = (a, b) => a === b): T[] {
+export function arrayUnique<T>(array: T[], compare: (a: T, b: T) => boolean = (a, b): boolean => a === b): T[] {
     return array.filter((v, i, a) => a.findIndex(x => compare(v, x)) === i);
 }
 
