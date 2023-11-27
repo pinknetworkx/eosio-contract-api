6d00d19fe85bda22f5409e31db7d5c48c453b228
Fabian Emilius <fabian.emilius@tum.de>
2023-11-27 20:10:37 +0100

    Fix marketplace name overflow

---
+++ definitions/migrations/1.3.22/atomicmarket.sql
@@ -0,0 +1,13 @@
+DROP VIEW atomicmarket_sales_master;
+DROP VIEW atomicmarket_auctions_master;
+DROP VIEW atomicmarket_buyoffers_master;
+
+ALTER TABLE atomicmarket_marketplaces ALTER COLUMN marketplace_name TYPE CHARACTER VARYING(13);
+ALTER TABLE atomicmarket_sales ALTER COLUMN maker_marketplace TYPE CHARACTER VARYING(13);
+ALTER TABLE atomicmarket_sales ALTER COLUMN taker_marketplace TYPE CHARACTER VARYING(13);
+ALTER TABLE atomicmarket_sales_filters ALTER COLUMN maker_marketplace TYPE CHARACTER VARYING(13);
+ALTER TABLE atomicmarket_sales_filters ALTER COLUMN taker_marketplace TYPE CHARACTER VARYING(13);
+ALTER TABLE atomicmarket_auctions ALTER COLUMN maker_marketplace TYPE CHARACTER VARYING(13);
+ALTER TABLE atomicmarket_auctions ALTER COLUMN taker_marketplace TYPE CHARACTER VARYING(13);
+ALTER TABLE atomicmarket_buyoffers ALTER COLUMN maker_marketplace TYPE CHARACTER VARYING(13);
+ALTER TABLE atomicmarket_buyoffers ALTER COLUMN taker_marketplace TYPE CHARACTER VARYING(13);
--- src/filler/handlers/atomicmarket/index.ts
+++ src/filler/handlers/atomicmarket/index.ts
@@ -137,6 +137,12 @@ export default class AtomicMarketHandler extends ContractHandler {
             await client.query(fs.readFileSync('./definitions/views/atomicmarket_buyoffers_master.sql', {encoding: 'utf8'}));
             await client.query(fs.readFileSync('./definitions/views/atomicmarket_sales_master.sql', {encoding: 'utf8'}));
         }
+
+        if (version === '1.3.22') {
+            await client.query(fs.readFileSync('./definitions/views/atomicmarket_auctions_master.sql', {encoding: 'utf8'}));
+            await client.query(fs.readFileSync('./definitions/views/atomicmarket_buyoffers_master.sql', {encoding: 'utf8'}));
+            await client.query(fs.readFileSync('./definitions/views/atomicmarket_sales_master.sql', {encoding: 'utf8'}));
+        }
     }
 
     constructor(filler: Filler, args: {[key: string]: any}) {
