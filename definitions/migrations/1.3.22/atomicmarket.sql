e232d5cd60ddb6bca60b2bc88d3ad8ad103567c5
Fabian Emilius <fabian.emilius@gmail.com>
2023-11-28 14:35:39 +0100

    Update definitions/migrations/1.3.22/atomicmarket.sql
    Co-authored-by: hugomspielworks <145326979+hugomspielworks@users.noreply.github.com>

--- definitions/migrations/1.3.22/atomicmarket.sql
+++ definitions/migrations/1.3.22/atomicmarket.sql
@@ -1,6 +1,7 @@
 DROP VIEW atomicmarket_sales_master;
 DROP VIEW atomicmarket_auctions_master;
 DROP VIEW atomicmarket_buyoffers_master;
+DROP VIEW atomicmarket_template_buyoffers_master;
 
 ALTER TABLE atomicmarket_marketplaces ALTER COLUMN marketplace_name TYPE CHARACTER VARYING(13);
 ALTER TABLE atomicmarket_sales ALTER COLUMN maker_marketplace TYPE CHARACTER VARYING(13);
